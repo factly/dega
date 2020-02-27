391b9e358caca5a936227e05d7a8d21b83dfa910
Monark <monark@factly.in>
2020-02-27 15:31:23 +0530

    ratings sorting added

--- graph/generated/generated.go
+++ graph/generated/generated.go
@@ -221,7 +221,7 @@ type ComplexityRoot struct {
 		Factchecks func(childComplexity int, categories []string, tags []string, users []string, page *int, limit *int) int
 		Formats    func(childComplexity int) int
 		Posts      func(childComplexity int, categories []string, tags []string, users []string, page *int, limit *int, sortBy *string, sortOrder *string) int
-		Ratings    func(childComplexity int, page *int, limit *int) int
+		Ratings    func(childComplexity int, page *int, limit *int, sortBy *string, sortOrder *string) int
 		Statuses   func(childComplexity int) int
 		Tags       func(childComplexity int, ids []string, page *int, limit *int, sortBy *string, sortOrder *string) int
 		Users      func(childComplexity int, page *int, limit *int, sortBy *string, sortOrder *string) int
@@ -337,7 +337,7 @@ type QueryResolver interface {
 	Statuses(ctx context.Context) (*models.StatusesPaging, error)
 	Posts(ctx context.Context, categories []string, tags []string, users []string, page *int, limit *int, sortBy *string, sortOrder *string) (*models.PostsPaging, error)
 	Users(ctx context.Context, page *int, limit *int, sortBy *string, sortOrder *string) (*models.UsersPaging, error)
-	Ratings(ctx context.Context, page *int, limit *int) (*models.RatingsPaging, error)
+	Ratings(ctx context.Context, page *int, limit *int, sortBy *string, sortOrder *string) (*models.RatingsPaging, error)
 	Claimants(ctx context.Context, page *int, limit *int) (*models.ClaimantsPaging, error)
 	Claims(ctx context.Context, ratings []string, claimants []string, page *int, limit *int) (*models.ClaimsPaging, error)
 	Factchecks(ctx context.Context, categories []string, tags []string, users []string, page *int, limit *int) (*models.FactchecksPaging, error)
@@ -1297,7 +1297,7 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 			return 0, false
 		}
 
-		return e.complexity.Query.Ratings(childComplexity, args["page"].(*int), args["limit"].(*int)), true
+		return e.complexity.Query.Ratings(childComplexity, args["page"].(*int), args["limit"].(*int), args["sortBy"].(*string), args["sortOrder"].(*string)), true
 
 	case "Query.statuses":
 		if e.complexity.Query.Statuses == nil {
@@ -1967,7 +1967,7 @@ type Query {
   statuses: StatusesPaging
   posts(categories: [String!], tags: [String!], users: [String!], page: Int, limit: Int, sortBy: String, sortOrder: String): PostsPaging
   users(page: Int, limit: Int, sortBy: String, sortOrder: String): UsersPaging
-  ratings(page: Int, limit: Int): RatingsPaging
+  ratings(page: Int, limit: Int, sortBy: String, sortOrder: String): RatingsPaging
   claimants(page: Int, limit: Int): ClaimantsPaging
   claims(ratings: [String!], claimants:[String!], page: Int, limit: Int): ClaimsPaging
   factchecks(categories: [String!], tags: [String!], users: [String!], page: Int, limit: Int): FactchecksPaging
@@ -2228,6 +2228,22 @@ func (ec *executionContext) field_Query_ratings_args(ctx context.Context, rawArg
 		}
 	}
 	args["limit"] = arg1
+	var arg2 *string
+	if tmp, ok := rawArgs["sortBy"]; ok {
+		arg2, err = ec.unmarshalOString2ᚖstring(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["sortBy"] = arg2
+	var arg3 *string
+	if tmp, ok := rawArgs["sortOrder"]; ok {
+		arg3, err = ec.unmarshalOString2ᚖstring(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["sortOrder"] = arg3
 	return args, nil
 }
 
@@ -6692,7 +6708,7 @@ func (ec *executionContext) _Query_ratings(ctx context.Context, field graphql.Co
 	fc.Args = args
 	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
 		ctx = rctx // use context from middleware stack in children
-		return ec.resolvers.Query().Ratings(rctx, args["page"].(*int), args["limit"].(*int))
+		return ec.resolvers.Query().Ratings(rctx, args["page"].(*int), args["limit"].(*int), args["sortBy"].(*string), args["sortOrder"].(*string))
 	})
 	if err != nil {
 		ec.Error(ctx, err)
--- graph/resolvers/rating.go
+++ graph/resolvers/rating.go
@@ -21,7 +21,7 @@ func (r *ratingResolver) Media(ctx context.Context, obj *models.Rating) (*models
 	return loaders.GetMediumLoader(ctx).Load(obj.Media.ID)
 }
 
-func (r *queryResolver) Ratings(ctx context.Context, page *int, limit *int) (*models.RatingsPaging, error) {
+func (r *queryResolver) Ratings(ctx context.Context, page *int, limit *int, sortBy *string, sortOrder *string) (*models.RatingsPaging, error) {
 	client := ctx.Value("client").(string)
 
 	if client == "" {
@@ -34,6 +34,9 @@ func (r *queryResolver) Ratings(ctx context.Context, page *int, limit *int) (*mo
 
 	pageLimit := 10
 	pageNo := 1
+	pageSortBy := "created_date"
+	pageSortOrder := -1
+
 	if limit != nil {
 		pageLimit = *limit
 	}
@@ -41,7 +44,14 @@ func (r *queryResolver) Ratings(ctx context.Context, page *int, limit *int) (*mo
 		pageNo = *page
 	}
 
-	opts := options.Find().SetSkip(int64((pageNo - 1) * pageLimit)).SetLimit(int64(pageLimit))
+	if sortBy != nil {
+		pageSortBy = *sortBy
+	}
+	if sortOrder != nil && *sortOrder == "ASC" {
+		pageSortOrder = 1
+	}
+
+	opts := options.Find().SetSort(bson.D{{pageSortBy, pageSortOrder}}).SetSkip(int64((pageNo - 1) * pageLimit)).SetLimit(int64(pageLimit))
 	cursor, err := mongo.Factcheck.Collection("rating").Find(ctx, query, opts)
 
 	if err != nil {
