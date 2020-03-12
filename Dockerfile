9fafccd887796af20036e36642fc3ba7bee20ca3
Monark <monark@factly.in>
2020-03-12 12:23:18 +0530

    dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM golang:latest
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+ENV PORT 8080
+ENV MONGO_URI mongodb://localhost:27017
+COPY . .
+RUN go build -o main .
+EXPOSE 8080
+CMD ["./main"]
--- graph/mongo/setup.go
+++ graph/mongo/setup.go
@@ -3,6 +3,7 @@ package mongo
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -15,11 +16,19 @@ var Core *mongo.Database
 // Factcheck mongodb collection
 var Factcheck *mongo.Database
 
+const defaultMongoURI = "mongodb://localhost:27017"
+
 // Setup to connect to db
 func Setup() {
 	log.Print("Initializing ...")
+
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		mongoURI = defaultMongoURI
+	}
+
 	var ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
-	var Client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	var Client, err = mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 
 	if err != nil {
 		log.Fatal(err)
