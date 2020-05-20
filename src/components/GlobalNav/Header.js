88c5bc1e003047919f0c4d0dcbd5f06d0fcac46c
Mahipat <mj@mojaave.com>
2020-05-20 09:57:03 +0530

    Added spaces dropdown in header

--- src/components/GlobalNav/Header.js
+++ src/components/GlobalNav/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Layout, AutoComplete, Divider, Row, Col, PageHeader } from 'antd';
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import { Layout, Menu, Dropdown, AutoComplete, Divider, Row, Col, PageHeader } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  UserOutlined,
+  MenuFoldOutlined,
+  DownOutlined,
+} from '@ant-design/icons';
 import { toggleSider } from '../../actions/settings';
 import SelectOrg from './SelectOrg';
 import AccountMenu from './AccountMenu';
+import { Link } from 'react-router-dom';
 
 const mockVal = (str, repeat = 1) => ({
   value: str.repeat(repeat),
@@ -27,6 +33,20 @@ function Header() {
   const onSelect = (data) => {
     console.log('onSelect', data);
   };
+  const menu = (
+    <Menu>
+      <Menu.Item key="0" icon={<DownOutlined />}>
+        Space 1
+      </Menu.Item>
+      <Menu.Item key="1" icon={<DownOutlined />}>
+        Space 2
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="3">
+        <Link to="/spaces/create">Create new space</Link>
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <Header className="layout-header">
       <Row>
@@ -43,6 +63,9 @@ function Header() {
                 onSearch={onSearch}
                 placeholder="Search....."
               />,
+              <Dropdown.Button overlay={menu} icon={<DownOutlined />}>
+                Selected Space
+              </Dropdown.Button>,
               <AccountMenu />,
               <SelectOrg />,
             ]}
