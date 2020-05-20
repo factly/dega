71ece537e0890f0145fab95c3c3a674c47f861b5
shreeharsha-factly <harsha@factly.in>
2020-05-20 14:47:50 +0530

    removed paging

--- service/core/action/tag/list.go
+++ service/core/action/tag/list.go
@@ -5,23 +5,14 @@ import (
 
 	"github.com/factly/dega-server/config"
 	"github.com/factly/dega-server/service/core/model"
-	"github.com/factly/dega-server/util"
 	"github.com/factly/dega-server/util/render"
 )
 
-// paging response
-type paging struct {
-	Total int         `json:"total"`
-	Nodes []model.Tag `json:"nodes"`
-}
-
 func list(w http.ResponseWriter, r *http.Request) {
 
-	data := paging{}
+	data := []model.Tag{}
 
-	offset, limit := util.Paging(r.URL.Query())
-
-	config.DB.Model(&model.Tag{}).Count(&data.Total).Offset(offset).Limit(limit).Find(&data.Nodes)
+	config.DB.Model(&model.Tag{}).Find(&data)
 
 	render.JSON(w, http.StatusOK, data)
 }
