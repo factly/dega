7aeef89d84a8087e08dbe3896855600534c03846
shreeharsha-factly <harsha@factly.in>
2020-05-20 14:54:38 +0530

    removed meta fields

--- service/factcheck/action/claim/create.go
+++ service/factcheck/action/claim/create.go
@@ -18,21 +18,18 @@ func create(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&claim)
 
 	result := &model.Claim{
-		Claim:           claim.Claim,
-		Slug:            claim.Slug,
-		ClaimDate:       claim.ClaimDate,
-		CheckedDate:     claim.CheckedDate,
-		ClaimSources:    claim.ClaimSources,
-		Description:     claim.Description,
-		ClaimantID:      claim.ClaimantID,
-		RatingID:        claim.RatingID,
-		Review:          claim.Review,
-		ReviewTagLine:   claim.ReviewTagLine,
-		ReviewSources:   claim.ReviewSources,
-		LastUpdatedByID: claim.LastUpdatedByID,
-		CreatedByID:     claim.CreatedByID,
-		MetaFields:      claim.MetaFields,
-		SpaceID:         claim.SpaceID,
+		Claim:         claim.Claim,
+		Slug:          claim.Slug,
+		ClaimDate:     claim.ClaimDate,
+		CheckedDate:   claim.CheckedDate,
+		ClaimSources:  claim.ClaimSources,
+		Description:   claim.Description,
+		ClaimantID:    claim.ClaimantID,
+		RatingID:      claim.RatingID,
+		Review:        claim.Review,
+		ReviewTagLine: claim.ReviewTagLine,
+		ReviewSources: claim.ReviewSources,
+		SpaceID:       claim.SpaceID,
 	}
 
 	err := config.DB.Model(&model.Claim{}).Create(&result).Error
