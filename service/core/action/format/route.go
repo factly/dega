9b77f318569a52317b81901b98892e0c41cd938c
shreeharsha-factly <harsha@factly.in>
2020-05-22 15:14:00 +0530

    additionalFields is removed

--- service/core/action/format/route.go
+++ service/core/action/format/route.go
@@ -2,16 +2,14 @@ package format
 
 import (
 	"github.com/go-chi/chi"
-	"github.com/jinzhu/gorm/dialects/postgres"
 )
 
 // format model
 type format struct {
-	Name             string         `json:"name" validate:"required"`
-	Slug             string         `json:"slug" validate:"required"`
-	Description      string         `json:"description"`
-	AdditionalFields postgres.Jsonb `json:"additional_fields"`
-	SpaceID          uint           `json:"space_id"`
+	Name        string `json:"name" validate:"required"`
+	Slug        string `json:"slug" validate:"required"`
+	Description string `json:"description"`
+	SpaceID     uint   `json:"space_id"`
 }
 
 // Router - Group of tag router
