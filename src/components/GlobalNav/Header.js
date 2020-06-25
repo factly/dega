92983e8348cddf3064438a3eea6267bb83122579
Monark <monark@factly.in>
2020-06-25 17:10:38 +0530

    search added

--- src/components/GlobalNav/Header.js
+++ src/components/GlobalNav/Header.js
@@ -1,68 +1,29 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Layout, Divider, Row, Col, Select, Avatar } from 'antd';
+import { Layout, Divider, Space } from 'antd';
 import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import { toggleSider } from '../../actions/settings';
 import AccountMenu from './AccountMenu';
-import { setSelectedSpace } from '../../actions/spaces';
-
-const { Option, OptGroup } = Select;
-
+import SpaceSelector from './SpaceSelector';
+import Search from './Search';
 function Header() {
   const { Header: HeaderAnt } = Layout;
 
-  const {
-    settings: {
-      sider: { collapsed },
-    },
-    spaces: { orgs, details, selected },
-  } = useSelector((state) => state);
+  const collapsed = useSelector((state) => state.settings.sider.collapsed);
   const dispatch = useDispatch();
   const MenuFoldComponent = collapsed ? MenuUnfoldOutlined : MenuFoldOutlined;
 
-  const handleSpaceChange = (space) => {
-    dispatch(setSelectedSpace(space));
-  };
   return (
     <HeaderAnt className="layout-header">
-      <Row>
-        <Col xs={2} sm={4}>
-          {
-            <MenuFoldComponent
-              style={{ fontSize: '20px' }}
-              className="trigger"
-              onClick={() => dispatch(toggleSider())}
-            />
-          }
-          <Divider type="vertical" />
-        </Col>
-        <Col xs={22} sm={20}>
-          <Select
-            style={{ width: 200 }}
-            value={selected}
-            onChange={handleSpaceChange}
-            bordered={false}
-          >
-            {orgs.map((organazation) => (
-              <OptGroup key={'org-' + organazation.id} label={organazation.title}>
-                {organazation.spaces.map((space) => (
-                  <Option key={'space-' + details[space].slug} value={details[space].id}>
-                    {details[space].logo ? (
-                      <Avatar size="small" src={details[space].logo.url} />
-                    ) : (
-                      <Avatar size="small" style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>
-                        {details[space].name[0].toUpperCase()}
-                      </Avatar>
-                    )}{' '}
-                    {details[space].name}
-                  </Option>
-                ))}
-              </OptGroup>
-            ))}
-          </Select>
-          <AccountMenu />
-        </Col>
-      </Row>
+      <Space direction="horizontal">
+        <MenuFoldComponent style={{ fontSize: '20px' }} onClick={() => dispatch(toggleSider())} />
+        <Divider type="vertical" />
+        <SpaceSelector />
+        <Divider type="vertical" />
+        <Search />
+        <Divider type="vertical" />
+        <AccountMenu />
+      </Space>
     </HeaderAnt>
   );
 }
