b3352026cec1ca9c3fba1346a70b865457026479
shreeharsha-factly <harsha@factly.in>
2020-07-11 17:54:16 +0530

    Removed factcheck constants

--- src/constants/factChecks.js
+++
@@ -1,9 +0,0 @@
-//Actions
-export const ADD_FACT_CHECK = 'ADD_FACT_CHECK';
-export const ADD_FACT_CHECKS = 'ADD_FACT_CHECKS';
-export const ADD_FACT_CHECKS_REQUEST = 'ADD_FACT_CHECKS_REQUEST';
-export const RESET_FACT_CHECKS = 'RESET_FACT_CHECKS';
-export const SET_FACT_CHECKS_LOADING = 'SET_FACT_CHECKS_LOADING';
-
-//API
-export const FACT_CHECKS_API = '/factcheck/factchecks';
