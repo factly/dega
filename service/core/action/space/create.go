a2ac2cd7608a29adc874302f7e7274ca680cb3c7
hetjagani <hetpatel572@gmail.com>
2020-07-28 13:49:08 +0530

    Rendering error responses wherever needed.

--- service/core/action/space/create.go
+++ service/core/action/space/create.go
@@ -100,6 +100,7 @@ func create(w http.ResponseWriter, r *http.Request) {
 	err = config.DB.Create(&result).Error
 
 	if err != nil {
+		errorx.Render(w, errorx.Parser(errorx.DBError()))
 		return
 	}
 
