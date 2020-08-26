800c8fb6dde4e2060aff7fdbe776886c401098ac
shreeharsha-factly <harsha@factly.in>
2020-08-26 16:36:14 +0530

    removed duplicated literals

--- service/core/model/migration.go
+++ service/core/model/migration.go
@@ -14,17 +14,20 @@ func Migration() {
 		&PostAuthor{},
 	)
 
-	config.DB.Model(&Category{}).AddForeignKey("medium_id", "media(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Category{}).AddForeignKey("space_id", "spaces(id)", "RESTRICT", "RESTRICT")
+	destMedium := "media(id)"
+	destSpace := "spaces(id)"
+
+	config.DB.Model(&Category{}).AddForeignKey("medium_id", destMedium, "RESTRICT", "RESTRICT")
+	config.DB.Model(&Category{}).AddForeignKey("space_id", destSpace, "RESTRICT", "RESTRICT")
 	config.DB.Model(&Category{}).AddForeignKey("parent_id", "categories(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Format{}).AddForeignKey("space_id", "spaces(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Medium{}).AddForeignKey("space_id", "spaces(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Space{}).AddForeignKey("logo_id", "media(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Space{}).AddForeignKey("logo_mobile_id", "media(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Space{}).AddForeignKey("fav_icon_id", "media(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Space{}).AddForeignKey("mobile_icon_id", "media(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Post{}).AddForeignKey("featured_medium_id", "media(id)", "RESTRICT", "RESTRICT")
+	config.DB.Model(&Format{}).AddForeignKey("space_id", destSpace, "RESTRICT", "RESTRICT")
+	config.DB.Model(&Medium{}).AddForeignKey("space_id", destSpace, "RESTRICT", "RESTRICT")
+	config.DB.Model(&Space{}).AddForeignKey("logo_id", destMedium, "RESTRICT", "RESTRICT")
+	config.DB.Model(&Space{}).AddForeignKey("logo_mobile_id", destMedium, "RESTRICT", "RESTRICT")
+	config.DB.Model(&Space{}).AddForeignKey("fav_icon_id", destMedium, "RESTRICT", "RESTRICT")
+	config.DB.Model(&Space{}).AddForeignKey("mobile_icon_id", destMedium, "RESTRICT", "RESTRICT")
+	config.DB.Model(&Post{}).AddForeignKey("featured_medium_id", destMedium, "RESTRICT", "RESTRICT")
 	config.DB.Model(&Post{}).AddForeignKey("format_id", "formats(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Post{}).AddForeignKey("space_id", "spaces(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Tag{}).AddForeignKey("space_id", "spaces(id)", "RESTRICT", "RESTRICT")
+	config.DB.Model(&Post{}).AddForeignKey("space_id", destSpace, "RESTRICT", "RESTRICT")
+	config.DB.Model(&Tag{}).AddForeignKey("space_id", destSpace, "RESTRICT", "RESTRICT")
 }
--- service/factcheck/model/migration.go
+++ service/factcheck/model/migration.go
@@ -10,14 +10,15 @@ func Migration() {
 		&Claim{},
 		&PostClaim{},
 	)
+	destSpace := "spaces(id)"
 
 	config.DB.Model(&Claimant{}).AddForeignKey("medium_id", "media(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Claimant{}).AddForeignKey("space_id", "spaces(id)", "RESTRICT", "RESTRICT")
+	config.DB.Model(&Claimant{}).AddForeignKey("space_id", destSpace, "RESTRICT", "RESTRICT")
 	config.DB.Model(&Rating{}).AddForeignKey("medium_id", "media(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Rating{}).AddForeignKey("space_id", "spaces(id)", "RESTRICT", "RESTRICT")
+	config.DB.Model(&Rating{}).AddForeignKey("space_id", destSpace, "RESTRICT", "RESTRICT")
 	config.DB.Model(&Claim{}).AddForeignKey("rating_id", "ratings(id)", "RESTRICT", "RESTRICT")
 	config.DB.Model(&Claim{}).AddForeignKey("claimant_id", "claimants(id)", "RESTRICT", "RESTRICT")
-	config.DB.Model(&Claim{}).AddForeignKey("space_id", "spaces(id)", "RESTRICT", "RESTRICT")
+	config.DB.Model(&Claim{}).AddForeignKey("space_id", destSpace, "RESTRICT", "RESTRICT")
 	config.DB.Model(&PostClaim{}).AddForeignKey("claim_id", "claims(id)", "RESTRICT", "RESTRICT")
 	config.DB.Model(&PostClaim{}).AddForeignKey("post_id", "posts(id)", "RESTRICT", "RESTRICT")
 }
