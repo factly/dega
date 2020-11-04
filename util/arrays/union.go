8fd066959df993ed73737e8e695335d0afeaa3d3
hetjagani <hetpatel572@gmail.com>
2020-11-04 10:46:40 +0530

    Fix: policies repeating for same action in /permissions response.

---
+++ util/arrays/union.go
@@ -0,0 +1,17 @@
+package arrays
+
+// Union returns union of two string slices
+func Union(a, b []string) []string {
+	m := make(map[string]bool)
+
+	for _, item := range a {
+		m[item] = true
+	}
+
+	for _, item := range b {
+		if _, ok := m[item]; !ok {
+			a = append(a, item)
+		}
+	}
+	return a
+}
