43d70b08ac9098eca16ff2ced396b9a48f0d1e4a
hetjagani <hetpatel572@gmail.com>
2020-11-13 18:42:37 +0530

    Resolved linting issue of unused var.

--- test/service/core/organisationPermission/testvars.go
+++ test/service/core/organisationPermission/testvars.go
@@ -31,23 +31,6 @@ var undecodableData = map[string]interface{}{
 	"spes":            5,
 }
 
-var permissionList = []map[string]interface{}{
-	{
-		"organisation_id": 1,
-		"spaces":          5,
-		"media":           10,
-		"posts":           10,
-		"fact_check":      true,
-	},
-	{
-		"organisation_id": 2,
-		"spaces":          3,
-		"media":           20,
-		"posts":           20,
-		"fact_check":      true,
-	},
-}
-
 var columns = []string{"id", "created_at", "updated_at", "deleted_at", "organisation_id", "spaces", "media", "posts", "fact_check"}
 
 var selectQuery = regexp.QuoteMeta(`SELECT * FROM "organisation_permissions"`)
