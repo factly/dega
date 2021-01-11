21ee0090ca8d707777bcd1c569e7c7159a5fb1c7
shreeharsha-factly <harsha@factly.in>
2021-01-11 16:18:57 +0530

    fix soft deleted are fetched with find query

--- graph/models/rating.go
+++ graph/models/rating.go
@@ -4,19 +4,21 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm/dialects/postgres"
+	"gorm.io/gorm"
 )
 
 // Rating model
 type Rating struct {
-	ID           uint           `gorm:"primary_key" json:"id"`
-	CreatedAt    time.Time      `json:"created_at"`
-	UpdatedAt    time.Time      `json:"updated_at"`
-	Name         string         `gorm:"column:name" json:"name"`
-	Slug         string         `gorm:"column:slug" json:"slug"`
-	Description  postgres.Jsonb `gorm:"column:description" json:"description"`
-	NumericValue int            `gorm:"column:numeric_value" json:"numeric_value"`
-	MediumID     uint           `gorm:"column:medium_id" json:"medium_id" sql:"DEFAULT:NULL"`
-	SpaceID      uint           `gorm:"column:space_id" json:"space_id"`
+	ID           uint            `gorm:"primary_key" json:"id"`
+	CreatedAt    time.Time       `json:"created_at"`
+	UpdatedAt    time.Time       `json:"updated_at"`
+	DeletedAt    *gorm.DeletedAt `sql:"index" json:"deleted_at"`
+	Name         string          `gorm:"column:name" json:"name"`
+	Slug         string          `gorm:"column:slug" json:"slug"`
+	Description  postgres.Jsonb  `gorm:"column:description" json:"description"`
+	NumericValue int             `gorm:"column:numeric_value" json:"numeric_value"`
+	MediumID     uint            `gorm:"column:medium_id" json:"medium_id" sql:"DEFAULT:NULL"`
+	SpaceID      uint            `gorm:"column:space_id" json:"space_id"`
 }
 
 // RatingsPaging model
