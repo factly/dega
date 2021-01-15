eb91e962386b268dfa9d2ae90e5f4f419ed24ff6
shreeharsha-factly <harsha@factly.in>
2021-01-15 15:24:30 +0530

    feat: add app menu in header

--- src/components/GlobalNav/Header.js
+++ src/components/GlobalNav/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Layout } from 'antd';
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import { Divider, Layout, Popover, List } from 'antd';
+import { MenuUnfoldOutlined, MenuFoldOutlined, AppstoreOutlined } from '@ant-design/icons';
 import { toggleSider } from '../../actions/settings';
 import AccountMenu from './AccountMenu';
 import SpaceSelector from './SpaceSelector';
 
-function Header() {
+function Header({ applications }) {
   const collapsed = useSelector((state) => state.settings.sider.collapsed);
   const dispatch = useDispatch();
   const MenuFoldComponent = collapsed ? MenuUnfoldOutlined : MenuFoldOutlined;
@@ -21,6 +21,44 @@ function Header() {
 
         <SpaceSelector />
 
+        {applications.length > 0 ? (
+          <>
+            <Divider type="vertical" />
+            <Popover
+              placement="bottom"
+              content={
+                <List
+                  grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 2,
+                    md: 4,
+                    lg: 4,
+                    xl: 6,
+                    xxl: 3,
+                  }}
+                  dataSource={applications}
+                  renderItem={(item) => (
+                    <List.Item>
+                      <a href={item.url} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <img
+                          alt="logo"
+                          className="menu-logo"
+                          src={require(`../../assets/${item.name.toLowerCase()}_icon.png`)}
+                        />
+                        <p>{item.name}</p>
+                      </a>
+                    </List.Item>
+                  )}
+                />
+              }
+              trigger="click"
+            >
+              <AppstoreOutlined />
+            </Popover>
+          </>
+        ) : null}
+        <Divider type="vertical" />
         <AccountMenu />
       </div>
     </Layout.Header>
