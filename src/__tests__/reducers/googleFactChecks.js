980e3ba612058eb0a58925a36dc0d05e70c75650
Bhumi101097 <bhumi.patel@factly.in>
2021-03-01 13:06:05 +0530

    added new unit test cases and fixed some exisiting

--- src/__tests__/reducers/googleFactChecks.js
+++ src/__tests__/reducers/googleFactChecks.js
@@ -25,6 +25,25 @@ const factCheck = {
     },
   ],
 };
+const existingFactCheck = {
+  text: 'Pelosi travels weekly to California on a 200-seat Boeing that costs millions annually in fuel',
+  claimant: 'social media users',
+  claimDate: '2021-02-21T00:00:00Z',
+  claimReview: [
+    {
+      publisher: {
+        name: 'BOOM',
+        site: 'boomlive.in',
+      },
+      url:
+        'https://www.usatoday.com/story/news/factcheck/2021/02/28/fact-check-false-claim-pelosi-travels-weekly-200-seat-plane/6860043002/',
+      title: 'Fact check: False claim that Pelosi travels weekly on 200-seat ...',
+      reviewDate: '2021-02-28T21:23:54Z',
+      textualRating: 'False',
+      languageCode: 'en',
+    },
+  ],
+}
 
 describe('google fact check reducer', () => {
   it('should return the initial state', () => {
@@ -77,6 +96,37 @@ describe('google fact check reducer', () => {
           data: [factCheck],
           query: { page: 1, limit: 1 },
           total: 1,
+        },
+      ],
+      loading: true,
+    });
+  });
+  it('should handle ADD_GOOGLE_FACT_CHECKS_REQUEST with some existing req', () => {
+    const state_ = {
+      req: [
+        {
+          data: [existingFactCheck],
+          query: { page: 1, limit: 5},
+          total: 1,
+        }
+      ],
+      loading: true,
+    }
+    expect(
+      reducer(state_, {
+        type: types.ADD_GOOGLE_FACT_CHECKS_REQUEST,
+        payload: {
+          data: [factCheck, existingFactCheck],
+          query: { page: 1, limit: 5 },
+          total: 2,
+        },
+      }),
+    ).toEqual({
+      req: [
+        {
+          data: [factCheck, existingFactCheck],
+          query: { page: 1, limit: 5 },
+          total: 2,
         },
       ],
       loading: true,
