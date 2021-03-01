57220eb3e5859ad554e076e514778be86567cb70
Bhumi101097 <bhumi.patel@factly.in>
2021-03-01 17:10:38 +0530

    Added and test cases for Header Component

--- src/components/GlobalNav/Header.test.js
+++ src/components/GlobalNav/Header.test.js
@@ -9,7 +9,7 @@ import { mount } from 'enzyme';
 import '../../matchMedia.mock';
 import Header from './Header';
 import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
-import * as actions from '../../actions/settings';
+import * as actions from '../../actions/sidebar';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -24,8 +24,8 @@ jest.mock('react-router-dom', () => ({
   useHistory: jest.fn(),
 }));
 
-jest.mock('../../actions/settings', () => ({
-  toggleSider: jest.fn(),
+jest.mock('../../actions/sidebar', () => ({
+  setCollapse: jest.fn(),
 }));
 
 describe('Header component', () => {
@@ -41,11 +41,20 @@ describe('Header component', () => {
       },
     },
     spaces: {
-      orgs: [],
-      details: {},
+      orgs: [{ id: 1, applications: [{ id: 1, name: 'App1', url: 'http://1233434/1323'}], spaces: [11]}],
+      details: {
+        11: {
+          id: 11,
+          organisation_id: 1,
+          name: 'Space 11',
+        }
+      },
       loading: true,
-      selected: 0,
+      selected: 11,
     },
+    sidebar: {
+      collapsed: true,
+    }
   };
   store = mockStore(() => state);
   store.dispatch = jest.fn(() => ({}));
@@ -56,51 +65,21 @@ describe('Header component', () => {
       const tree = renderer
         .create(
           <Provider store={store}>
-            <Header />
+            <Header applications={[{ id: 1, name: 'App1', url: 'http://1233434/1323'}]}/>
           </Provider>,
         )
         .toJSON();
       expect(tree).toMatchSnapshot();
     });
-  });
-  describe('component testing', () => {
-    let wrapper;
-    it('should call MenuUnfoldOutlined', (done) => {
-      actions.toggleSider.mockReset();
-      const push = jest.fn();
-      useHistory.mockReturnValueOnce({ push });
-
-      wrapper = mount(
-        <Provider store={store}>
-          <Header />
-        </Provider>,
-      );
-
-      wrapper.find(MenuUnfoldOutlined).props().onClick();
-      wrapper.update();
-
-      expect(actions.toggleSider).toHaveBeenCalledWith();
-      done();
-    });
-    it('should call MenuFoldOutlined', (done) => {
-      actions.toggleSider.mockReset();
-      const push = jest.fn();
-      useHistory.mockReturnValueOnce({ push });
-
-      state.settings.sider.collapsed = false;
-      store = mockStore(() => state);
-
-      wrapper = mount(
-        <Provider store={store}>
-          <Header />
-        </Provider>,
-      );
-
-      wrapper.find(MenuFoldOutlined).props().onClick();
-      wrapper.update();
-
-      expect(actions.toggleSider).toHaveBeenCalledWith();
-      done();
+    it('should render the component with no applications', () => {
+      const tree = renderer
+        .create(
+          <Provider store={store}>
+            <Header applications={[ ]}/>
+          </Provider>,
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
     });
   });
 });
