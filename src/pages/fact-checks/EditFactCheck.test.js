bbfff3aadcf5cacda5be15a16d1334527ecc84c0
Bhumi101097 <bhumi.patel@factly.in>
2021-03-09 15:01:07 +0530

    Added Test cases for Factcheck

---
+++ src/pages/fact-checks/EditFactCheck.test.js
@@ -0,0 +1,266 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { act } from 'react-dom/test-utils';
+import { shallow, mount } from 'enzyme';
+
+import '../../matchMedia.mock';
+import EditFactCheck from './EditFactCheck';
+import * as actions from '../../actions/posts';
+import FactCheckEditForm from './components/FactCheckForm';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+jest.mock('@editorjs/editorjs');
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(),
+}));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: jest.fn(),
+  useParams: jest.fn().mockReturnValue({ id: '1' }),
+}));
+
+jest.mock('../../actions/posts', () => ({
+  getPosts: jest.fn(),
+  getPost: jest.fn(),
+  updatePost: jest.fn(),
+  addPost: jest.fn(),
+  publishPost: jest.fn(),
+  addTemplate: jest.fn(),
+}));
+const formats =  {
+  article: {id: 1, name: 'article', slug: 'article'},
+  factcheck: {id: 2, name: 'factcheck', slug: 'fact-check'},
+  loading: false,
+};
+let state = {
+  posts: {
+    req: [],
+    details: {},
+    loading: false,
+  },
+  authors: {
+    req: [],
+    details: {},
+    loading: true,
+  },
+  tags: {
+    req: [],
+    details: {},
+    loading: true,
+  },
+  categories: {
+    req: [],
+    details: {},
+    loading: true,
+  },
+  formats: {
+    req: [],
+    details: {},
+    loading: true,
+  },
+
+  claims: {
+    req: [],
+    details: {},
+    loading: true,
+  },
+  claimants: {
+    req: [],
+    details: {},
+    loading: true,
+  },
+  ratings: {
+    req: [],
+    details: {},
+    loading: true,
+  },
+  media: {
+    req: [],
+    details: {},
+    loading: true,
+  },
+  spaces: {
+    orgs: [{ id: 1, organazation: 'Organization 1', spaces: [11] }],
+    details: {
+      11: { id: 11, name: 'Space 11' },
+    },
+    loading: false,
+    selected: 11,
+  },
+  sidebar: {
+    collapsed: false,
+  }
+};
+
+describe('FactCheck edit component', () => {
+  let store;
+  let mockedDispatch;
+  store = mockStore(state);
+  store.dispatch = jest.fn(() => ({}));
+  mockedDispatch = jest.fn(() => Promise.resolve({}));
+  useDispatch.mockReturnValue(mockedDispatch);
+
+  describe('snapshot testing', () => {
+    it('should render the component', () => {
+      let tree;
+      act(() => {
+        tree = mount(
+          <Provider store={store}>
+            <EditFactCheck formats={formats} />
+          </Provider>,
+        );
+      });
+      expect(tree).toMatchSnapshot();
+    });
+    it('should match skeleton while loading', () => {
+      let tree;
+      store = mockStore({
+        posts: {
+          req: [],
+          details: {},
+          loading: true,
+        },
+        spaces: {
+          orgs: [{ id: 1, organazation: 'Organization 1', spaces: [11] }],
+          details: {
+            11: { id: 11, name: 'Space 11' },
+          },
+          loading: false,
+          selected: 11,
+        },
+        sidebar: {
+          collapsed: false,
+        }
+      });
+      act(() => {
+        tree = mount(
+          <Provider store={store}>
+            <EditFactCheck formats={formats}  />
+          </Provider>,
+        );
+      });
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  describe('component testing', () => {
+    let wrapper;
+    state.posts = {
+      req: [],
+      details: {
+        1: {
+          id: 1,
+          title: 'FactCheck-1',
+          slug: 'factcheck-1',
+          tag_line: 'tag_line',
+          medium_id: 1,
+          format_id: 2,
+        },
+      },
+      loading: false,
+    };
+    beforeEach(() => {
+      store = mockStore(state);
+    });
+    afterEach(() => {
+      wrapper.unmount();
+    });
+    it('should call get action', () => {
+      actions.getPost.mockReset();
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <EditFactCheck formats={formats} />
+          </Provider>,
+        );
+      });
+      expect(actions.getPost).toHaveBeenCalledWith('1');
+    });
+    it('should call updatePost', (done) => {
+      const push = jest.fn();
+      useHistory.mockReturnValueOnce({ push });
+      actions.updatePost.mockReset();
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <EditFactCheck formats={formats} />
+          </Provider>,
+        );
+      });
+
+      wrapper.find(FactCheckEditForm).props().onCreate({ status: 'draft' });
+      setTimeout(() => {
+        expect(actions.updatePost).toHaveBeenCalledWith({
+          id: 1,
+          title: 'FactCheck-1',
+          slug: 'factcheck-1',
+          tag_line: 'tag_line',
+          medium_id: 1,
+          format_id: 2,
+          status: 'draft',
+        });
+        expect(push).toHaveBeenCalledWith('/fact-checks');
+        done();
+      }, 0);
+    });
+    it('should call publishPost', (done) => {
+      const push = jest.fn();
+      useHistory.mockReturnValueOnce({ push });
+      actions.publishPost.mockReset();
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <EditFactCheck formats={formats}  />
+          </Provider>,
+        );
+      });
+
+      wrapper.find(FactCheckEditForm).props().onCreate({ status: 'publish' });
+      setTimeout(() => {
+        expect(actions.publishPost).toHaveBeenCalledWith({
+          id: 1,
+          title: 'FactCheck-1',
+          slug: 'factcheck-1',
+          tag_line: 'tag_line',
+          medium_id: 1,
+          format_id: 2,
+          status: 'publish',
+        });
+        expect(push).toHaveBeenCalledWith('/fact-checks');
+        done();
+      }, 0);
+    });
+    it('should display RecordNotFound if post not found', () => {
+      state.posts = {
+        req: [],
+        details: {
+          2: {
+            id: 2,
+            title: 'Factcheck-2',
+            slug: 'factcheck-2',
+            tag_line: 'tag_line',
+            medium_id: 1,
+            format_id: 2,
+          },
+        },
+        loading: false,
+      };
+      store = mockStore(state);
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <EditFactCheck formats={formats}  />
+          </Provider>,
+        );
+      });
+      expect(wrapper.find('RecordNotFound').length).toBe(1);
+      expect(wrapper.find(FactCheckEditForm).length).toBe(0);
+    });
+  });
+});
---
+++ src/pages/fact-checks/GoogleFactCheck.test.js
@@ -0,0 +1,205 @@
+import React from 'react';
+import { useDispatch, Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import { List } from 'antd';
+
+import '../../matchMedia.mock';
+import GoogleFactCheck from './GoogleFactCheck';
+import { getGoogleFactChecks } from '../../actions/googleFactChecks';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+let mockedDispatch, store;
+
+let state = {
+  googleFactChecks: {
+    req: [
+      {
+        data: [
+          {
+            text: 'Video shows Russian doctors celebrating the new COVID-19 vaccine.',
+            claimant: 'Facebook posts',
+            claimDate: '2020-09-08T02:48:10Z',
+            claimReview: [
+              {
+                publisher: {
+                  name: 'BOOM',
+                  site: 'boomlive.in',
+                },
+                url:
+                  'https://www.boomlive.in/world/video-from-saudi-arabia-shared-as-russian-doctors-celebrating-covid-19-vaccine-9654',
+                title: 'Video From Saudi Arabia Shared As Russian Doctors Celebrating COVID-19 Vaccine',
+                reviewDate: '2020-09-08T02:48:10Z',
+                textualRating: 'False',
+                languageCode: 'en',
+              },
+            ],
+          },
+          {
+            text: 'Pelosi travels weekly to California on a 200-seat Boeing that costs millions annually in fuel',
+            claimant: 'social media users',
+            claimDate: '2021-02-21T00:00:00Z',
+            claimReview: [
+              {
+                publisher: {
+                  name: 'BOOM',
+                  site: 'boomlive.in',
+                },
+                url:
+                  'https://www.usatoday.com/story/news/factcheck/2021/02/28/fact-check-false-claim-pelosi-travels-weekly-200-seat-plane/6860043002/',
+                title: 'Fact check: False claim that Pelosi travels weekly on 200-seat ...',
+                reviewDate: '2021-02-28T21:23:54Z',
+                textualRating: 'False',
+                languageCode: 'en',
+              },
+            ],
+          }, 
+        ],  
+        query: {
+          page: 1,
+          query: 'factcheck',
+        },
+        total: 2,
+      }
+    ],
+    loading:  false,
+  },
+};
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(),
+}));
+jest.mock('../../actions/googleFactChecks', () => ({
+  getGoogleFactChecks: jest.fn(),
+}));
+describe('GoogleFactCheck component', () => {
+  describe('snapshot testing', () => {
+    beforeEach(() => {
+      store = mockStore({});
+      store.dispatch = jest.fn();
+      mockedDispatch = jest.fn();
+      useDispatch.mockReturnValue(mockedDispatch);
+    });
+    it('should render the component', () => {
+      store = mockStore(state);
+      const tree = mount(
+        <Provider store={store}>
+          <GoogleFactCheck />
+        </Provider>,
+      );
+      expect(tree).toMatchSnapshot();
+    });
+    it('should match when component loading', () => {
+      state.googleFactChecks.loading = true;
+      store = mockStore(state);
+      const tree = mount(
+        <Provider store={store}>
+          <GoogleFactCheck />
+        </Provider>,
+      );
+      expect(tree).toMatchSnapshot();
+    });
+    it('should match when data', () => {
+      state.googleFactChecks.loading = false;
+      store = mockStore(state);
+      const tree = mount(
+        <Provider store={store}>
+          <GoogleFactCheck />
+        </Provider>,
+      );
+      expect(tree).toMatchSnapshot();
+      expect(getGoogleFactChecks).toHaveBeenCalledWith({ page: 1, query: 'factcheck' });
+    });
+  });
+  describe('component testing', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      mockedDispatch = jest.fn(() => new Promise((resolve)=> resolve(true)));
+      useDispatch.mockReturnValue(mockedDispatch);
+    });
+    it('should change the page', () => {
+      store = mockStore(state);
+      let wrapper;
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <GoogleFactCheck />
+          </Provider>,
+        );
+      });
+      const list = wrapper.find(List);
+      list.props().pagination.onChange(3);
+      wrapper.update();
+      const updatedList = wrapper.find(List);
+      expect(updatedList.props().pagination.current).toEqual(3);
+    });
+    it('should submit filters', () => {
+      store = mockStore(state);
+      let wrapper;
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <GoogleFactCheck />
+          </Provider>,
+        );
+        wrapper
+          .find('FormItem')
+          .at(0)
+          .find('Input')
+          .simulate('change', { target: { value: 'Russian doctors' } });
+        wrapper
+          .find('FormItem')
+          .at(1)
+          .find('Select')
+          .at(0)
+          .props()
+          .onChange({ target: { value: 'en'} });
+       
+        const submitButtom = wrapper.find('Button').at(0);
+        submitButtom.simulate('submit');
+      });
+      setTimeout(() => {
+        expect(getGoogleFactChecks).toHaveBeenCalledWith({
+          page: 1,
+          q: 'Russian doctors',
+        });
+      }, 0);
+    });
+    it('should submit with language as all', () => {
+      store = mockStore(state);
+      let wrapper;
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <GoogleFactCheck />
+          </Provider>,
+        );
+        wrapper
+          .find('FormItem')
+          .at(0)
+          .find('Input')
+          .simulate('change', { target: { value: 'Russian doctors' } });
+        wrapper
+          .find('FormItem')
+          .at(1)
+          .find('Select')
+          .at(0)
+          .props()
+          .onChange({ target: { value: 'all'} });
+       
+        const submitButtom = wrapper.find('Button').at(0);
+        submitButtom.simulate('submit');
+      });
+      setTimeout(() => {
+        expect(getGoogleFactChecks).toHaveBeenCalledWith({
+          page: 1,
+          q: 'Russian doctors',
+          language: 'all',
+        });
+      }, 0);
+    });
+  });
+});
