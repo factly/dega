580ac8e1fe0dc43a24070d7d0b2feff2be83cf61
Bhumi101097 <bhumi.patel@factly.in>
2021-03-30 17:33:10 +0530

    Remove default section if some existing values

--- src/pages/dashboard/components/Features.js
+++ src/pages/dashboard/components/Features.js
@@ -31,61 +31,65 @@ function Features() {
 
   return (
     <>
-      <Typography.Title level={3}>Add default features</Typography.Title>
+      {ratings > 0 || formats > 0 || policies > 0 ? null : (
+        <Typography.Title level={3}>Add default features</Typography.Title>
+      )}
 
       <Space>
-        <Card
-          title="Ratings"
-          actions={[
-            <Button
-              onClick={() => {
-                dispatch(addDefaultRatings()).then(() => history.push('/ratings'));
-              }}
-              disabled={ratings > 0}
-            >
-              <PlusOutlined /> CREATE RATINGS
-            </Button>,
-          ]}
-          style={{ width: 300 }}
-        >
-          Five ratings will be created True, Partly True, Misleading, Partly False and False. Click
-          below Button to create
-        </Card>
+        {ratings > 0 ? null : (
+          <Card
+            title="Ratings"
+            actions={[
+              <Button
+                onClick={() => {
+                  dispatch(addDefaultRatings()).then(() => history.push('/ratings'));
+                }}
+              >
+                <PlusOutlined /> CREATE RATINGS
+              </Button>,
+            ]}
+            style={{ width: 300 }}
+          >
+            Five ratings will be created True, Partly True, Misleading, Partly False and False.
+            Click below Button to create
+          </Card>
+        )}
+        {formats > 0 ? null : (
+          <Card
+            title="Formats"
+            actions={[
+              <Button
+                onClick={() => {
+                  dispatch(addDefaultFormats()).then(() => history.push('/formats'));
+                }}
+              >
+                <PlusOutlined /> CREATE FORMATS
+              </Button>,
+            ]}
+            style={{ width: 300 }}
+          >
+            Two formats will be created Fact Check and Article. Click below Button to create
+          </Card>
+        )}
 
-        <Card
-          title="Formats"
-          actions={[
-            <Button
-              onClick={() => {
-                dispatch(addDefaultFormats()).then(() => history.push('/formats'));
-              }}
-              disabled={formats > 0}
-            >
-              <PlusOutlined /> CREATE FORMATS
-            </Button>,
-          ]}
-          style={{ width: 300 }}
-        >
-          Two formats will be created Fact Check and Article. Click below Button to create
-        </Card>
-
-        <Card
-          title="Policies"
-          actions={[
-            <Button
-              onClick={() => {
-                dispatch(addDefaultPolicies()).then(() => history.push('/policies'));
-              }}
-              disabled={policies > 0}
-            >
-              <PlusOutlined /> CREATE POLICIES
-            </Button>,
-          ]}
-          style={{ width: 300 }}
-        >
-          Three policies will be created Editor, Author and Contributor. Click below Button to
-          create
-        </Card>
+        {policies > 0 ? null : (
+          <Card
+            title="Policies"
+            actions={[
+              <Button
+                onClick={() => {
+                  dispatch(addDefaultPolicies()).then(() => history.push('/policies'));
+                }}
+              >
+                <PlusOutlined /> CREATE POLICIES
+              </Button>,
+            ]}
+            style={{ width: 300 }}
+          >
+            Three policies will be created Editor, Author and Contributor. Click below Button to
+            create
+          </Card>
+        )}
       </Space>
     </>
   );
