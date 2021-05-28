caa2aab232c7b870eff6950b5a412c96363069f1
Bhumi101097 <bhumi.patel@factly.in>
2021-05-28 17:32:41 +0530

    Added filter for pages

--- service/core/action/page/list.go
+++ service/core/action/page/list.go
@@ -3,12 +3,15 @@ package page
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"strconv"
 
 	"github.com/factly/dega-server/config"
 	"github.com/factly/dega-server/service/core/action/author"
 	"github.com/factly/dega-server/service/core/model"
 	"github.com/factly/x/errorx"
 	"github.com/factly/x/loggerx"
+	"github.com/factly/x/meilisearchx"
 	"github.com/factly/x/middlewarex"
 	"github.com/factly/x/paginationx"
 	"github.com/factly/x/renderx"
@@ -40,10 +43,47 @@ func list(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Filters
+	u, _ := url.Parse(r.URL.String())
+	queryMap := u.Query()
+	searchQuery := r.URL.Query().Get("q")
 	sort := r.URL.Query().Get("sort")
+
+	filters := generateFilters(queryMap["tag"], queryMap["category"], queryMap["author"], queryMap["status"])
+	filteredPageIDs := make([]uint, 0)
+
+	if filters != "" {
+		filters = fmt.Sprint(filters, " AND space_id=", sID)
+	}
 
 	result := paging{}
 	result.Nodes = make([]pageData, 0)
+
+	if filters != "" || searchQuery != "" {
+		// Search pages with filter
+		var hits []interface{}
+		var res map[string]interface{}
+
+		if searchQuery != "" {
+			hits, err = meilisearchx.SearchWithQuery("dega", searchQuery, filters, "page")
+		} else {
+			res, err = meilisearchx.SearchWithoutQuery("dega", filters, "page")
+			if _, found := res["hits"]; found {
+				hits = res["hits"].([]interface{})
+			}
+		}
+		if err != nil {
+			loggerx.Error(err)
+			renderx.JSON(w, http.StatusOK, result)
+			return
+		}
+
+		filteredPageIDs = meilisearchx.GetIDArray(hits)
+		if len(filteredPageIDs) == 0 {
+			renderx.JSON(w, http.StatusOK, result)
+			return
+		}
+	}
 
 	posts := make([]model.Post, 0)
 	offset, limit := paginationx.Parse(r.URL.Query())
@@ -52,9 +92,31 @@ func list(w http.ResponseWriter, r *http.Request) {
 		sort = "desc"
 	}
 
-	config.DB.Preload("Medium").Preload("Format").Preload("Tags").Preload("Categories").Model(&model.Post{}).Where(&model.Post{
+	tx := config.DB.Preload("Medium").Preload("Format").Preload("Tags").Preload("Categories").Model(&model.Post{}).Where(&model.Post{
 		SpaceID: uint(sID),
 	}).Where("is_page = ?", true).Order("created_at " + sort).Count(&result.Total).Offset(offset).Limit(limit).Find(&posts)
+
+	formatIDs := make([]uint, 0)
+	for _, fid := range queryMap["format"] {
+		fidStr, _ := strconv.Atoi(fid)
+		formatIDs = append(formatIDs, uint(fidStr))
+	}
+
+	if len(formatIDs) > 0 {
+		tx.Where("format_id IN (?)", formatIDs)
+	}
+
+	if len(filteredPageIDs) > 0 {
+		err = tx.Where(filteredPageIDs).Count(&result.Total).Offset(offset).Limit(limit).Find(&posts).Error
+	} else {
+		err = tx.Where("status != ?", "template").Count(&result.Total).Offset(offset).Limit(limit).Find(&posts).Error
+	}
+
+	if err != nil {
+		loggerx.Error(err)
+		errorx.Render(w, errorx.Parser(errorx.DBError()))
+		return
+	}
 
 	var postIDs []uint
 	for _, p := range posts {
@@ -101,4 +163,28 @@ func list(w http.ResponseWriter, r *http.Request) {
 	}
 
 	renderx.JSON(w, http.StatusOK, result)
+}
+func generateFilters(tagIDs, categoryIDs, authorIDs, status []string) string {
+	filters := ""
+	if len(tagIDs) > 0 {
+		filters = fmt.Sprint(filters, meilisearchx.GenerateFieldFilter(tagIDs, "tag_ids"), " AND ")
+	}
+
+	if len(categoryIDs) > 0 {
+		filters = fmt.Sprint(filters, meilisearchx.GenerateFieldFilter(categoryIDs, "category_ids"), " AND ")
+	}
+
+	if len(authorIDs) > 0 {
+		filters = fmt.Sprint(filters, meilisearchx.GenerateFieldFilter(authorIDs, "author_ids"), " AND ")
+	}
+
+	if len(status) > 0 {
+		filters = fmt.Sprint(filters, meilisearchx.GenerateFieldFilter(status, "status"), " AND ")
+	}
+
+	if filters != "" && filters[len(filters)-5:] == " AND " {
+		filters = filters[:len(filters)-5]
+	}
+
+	return filters
 }
