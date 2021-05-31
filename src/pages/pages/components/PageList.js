dc9b2e07e1627215019f9fb2f57be0479d904992
Bhumi101097 <bhumi.patel@factly.in>
2021-05-31 19:38:19 +0530

    Ui-filter changes for pages

--- src/pages/pages/components/PageList.js
+++ src/pages/pages/components/PageList.js
@@ -1,173 +1,34 @@
 import React, { useState } from 'react';
-import { Popconfirm, Button, List, Input, Select, Form, Space, Tag, Row, Col } from 'antd';
+import { Popconfirm, Button, List, Space, Tag } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import { useDispatch, useSelector } from 'react-redux';
-import { getPages, deletePage } from '../../../actions/pages';
+import { useDispatch } from 'react-redux';
+import { deletePage } from '../../../actions/pages';
 import { Link } from 'react-router-dom';
-import Selector from '../../../components/Selector';
-import deepEqual from 'deep-equal';
 import ImagePlaceholder from '../../../components/ErrorsAndImage/PlaceholderImage';
 import QuickEdit from '../../../components/List/QuickEdit';
 import moment from 'moment';
 
-function PageList({ actions, format, status }) {
+function PageList({ actions, format, status, data, filters, setFilters, fetchPages }) {
   const dispatch = useDispatch();
-  const { Option } = Select;
-  const [form] = Form.useForm();
-
-  const [filters, setFilters] = React.useState({
-    page: 1,
-    limit: 20,
-  });
   const [id, setID] = useState(0);
 
-  const { pages, total, loading, tags, categories } = useSelector((state) => {
-    const node = state.pages.req.find((item) => {
-      return deepEqual(item.query, filters);
-    });
-
-    if (node)
-      return {
-        pages: node.data.map((element) => {
-          const page = state.pages.details[element];
-
-          page.medium = state.media.details[page.featured_medium_id];
-          return page;
-        }),
-        total: node.total,
-        loading: state.pages.loading,
-        tags: state.tags.details,
-        categories: state.categories.details,
-      };
-    return { pages: [], total: 0, loading: state.pages.loading, tags: {}, categories: {} };
-  });
-
-  React.useEffect(() => {
-    fetchPages();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filters]);
-
-  const fetchPages = () => {
-    dispatch(getPages(filters));
-  };
-
-  const onSave = (values) => {
-    let filterValue = {
-      tag: values.tags,
-      category: values.categories,
-      sort: values.sort,
-      q: values.q,
-      author: values.authors,
-      status: values.status !== 'all' ? values.status : null,
-    };
-    setFilters({ ...filters, ...filterValue });
-  };
-
   const getTagList = (tagids) => {
-    return tagids.map((id) => <Tag>{tags[id].name}</Tag>);
+    return tagids.map((id) => <Tag>{data.tags[id].name}</Tag>);
   };
   const getCategoryList = (catIds) => {
-    return catIds.map((id) => <Tag>{categories[id].name}</Tag>);
+    return catIds.map((id) => <Tag>{data.categories[id].name}</Tag>);
   };
 
   return (
     <Space direction="vertical">
-      <Form
-        initialValues={filters}
-        form={form}
-        name="filters"
-        layout="inline"
-        onFinish={(values) => onSave(values)}
-        style={{ maxWidth: '100%' }}
-        className="ant-advanced-search-form"
-      >
-        <Row gutter={24}>
-          <Col span={8} key={1}>
-            <Form.Item name="q" label="Search" style={{ width: '260px' }}>
-              <Input placeholder="search Pages" />
-            </Form.Item>
-          </Col>
-          <Col span={8} key={2}>
-            <Form.Item name="sort" label="Sort" style={{ width: '260px' }}>
-              <Select defaultValue="desc" style={{ maxWidth: '260px' }}>
-                <Option value="desc">Latest</Option>
-                <Option value="asc">Old</Option>
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={8} key={3}>
-            <Form.Item name="status" label="Status" style={{ width: '260px' }}>
-              <Select defaultValue="all" style={{ maxWidth: '260px' }}>
-                <Option value="all">All</Option>
-                <Option value="draft">Draft</Option>
-                <Option value="publish">Publish</Option>
-                <Option value="ready">Ready to Publish</Option>
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={8} key={4}>
-            <Form.Item name="tags" label="Tags" style={{ width: '260px' }}>
-              <Selector
-                mode="multiple"
-                action="Tags"
-                placeholder="Filter Tags"
-                style={{
-                  maxWidth: '260px',
-                  marginTop: '8px',
-                }}
-              />
-            </Form.Item>
-          </Col>
-          <Col span={8} key={5}>
-            <Form.Item name="categories" label="Categories" style={{ width: '260px' }}>
-              <Selector
-                mode="multiple"
-                action="Categories"
-                placeholder="Filter Categories"
-                style={{
-                  maxWidth: '260px',
-                  marginTop: '8px',
-                }}
-              />
-            </Form.Item>
-          </Col>
-          <Col span={8} key={6}>
-            <Form.Item name="authors" label="Authors" style={{ width: '260px' }}>
-              <Selector
-                mode="multiple"
-                action="Authors"
-                placeholder="Filter Authors"
-                display={'email'}
-                style={{
-                  maxWidth: '260px',
-                  marginTop: '8px',
-                }}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={24}>
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                style={{ marginTop: '8px', marginLeft: '1080px' }}
-              >
-                Submit
-              </Button>
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
       <List
         bordered
         className="post-list"
-        loading={loading}
+        loading={data.loading}
         itemLayout="vertical"
-        dataSource={pages}
+        dataSource={data.pages}
         pagination={{
-          total: total,
+          total: data.total,
           current: filters.page,
           pageSize: filters.limit,
           onChange: (pageNumber, pageSize) =>
--- src/pages/pages/index.js
+++ src/pages/pages/index.js
@@ -1,23 +1,155 @@
 import React from 'react';
-import { Space, Button } from 'antd';
+import { Space, Button, Select, Form, Col, Row, Input } from 'antd';
 import { Link } from 'react-router-dom';
 import PageList from './components/PageList';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import getUserPermission from '../../utils/getUserPermission';
 import FormatNotFound from '../../components/ErrorsAndImage/RecordNotFound';
+import deepEqual from 'deep-equal';
+import { getPages } from '../../actions/pages';
+import Selector from '../../components/Selector';
 
 function Pages({ formats }) {
   const spaces = useSelector(({ spaces }) => spaces);
   const actions = getUserPermission({ resource: 'pages', action: 'get', spaces });
+  const { Option } = Select;
+  const [form] = Form.useForm();
+  const dispatch = useDispatch();
+  const [filters, setFilters] = React.useState({
+    page: 1,
+    limit: 20,
+  });
+  const { pages, total, loading, tags, categories } = useSelector((state) => {
+    const node = state.pages.req.find((item) => {
+      return deepEqual(item.query, filters);
+    });
+
+    if (node)
+      return {
+        pages: node.data.map((element) => {
+          const page = state.pages.details[element];
+
+          page.medium = state.media.details[page.featured_medium_id];
+          return page;
+        }),
+        total: node.total,
+        loading: state.pages.loading,
+        tags: state.tags.details,
+        categories: state.categories.details,
+      };
+    return { pages: [], total: 0, loading: state.pages.loading, tags: {}, categories: {} };
+  });
+
+  React.useEffect(() => {
+    fetchPages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filters]);
+
+  const fetchPages = () => {
+    dispatch(getPages(filters));
+  };
+
+  const onSave = (values) => {
+    let filterValue = {
+      tag: values.tags,
+      category: values.categories,
+      sort: values.sort,
+      q: values.q,
+      author: values.authors,
+      status: values.status !== 'all' ? values.status : null,
+    };
+    setFilters({ ...filters, ...filterValue });
+  };
   if (!formats.loading && formats.article)
     return (
       <Space direction="vertical">
-        <Link key="1" to="/pages/create">
-          <Button disabled={!(actions.includes('admin') || actions.includes('create'))}>
-            Create New
-          </Button>
-        </Link>
-        <PageList actions={actions} format={formats.article} />
+        <Form
+          initialValues={filters}
+          form={form}
+          name="filters"
+          onFinish={(values) => onSave(values)}
+          style={{ maxWidth: '100%' }}
+          className="ant-advanced-search-form"
+          onValuesChange={(changedValues, allValues) => {
+            console.log('changedValues', changedValues, 'all', allValues);
+            if (!changedValues.q) {
+              onSave(allValues);
+            }
+          }}
+        >
+          <Row gutter={24}>
+            <Col key={1}>
+              <Link to="/pages/create">
+                <Button disabled={!(actions.includes('admin') || actions.includes('create'))}>
+                  Create New
+                </Button>
+              </Link>
+            </Col>
+            <Col key={2} span={9} offset={12}>
+              <Space direction="horizontal">
+                <Form.Item name="q">
+                  <Input placeholder="Search pages" />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button htmlType="submit">Search</Button>
+                </Form.Item>
+                <Form.Item name="sort" label="Sort">
+                  <Select defaultValue="desc" style={{ width: '100%' }}>
+                    <Option value="desc">Latest</Option>
+                    <Option value="asc">Old</Option>
+                  </Select>
+                </Form.Item>
+              </Space>
+            </Col>
+          </Row>
+          <Row gutter={10}>
+            <Col span={4} key={4}>
+              <Form.Item name="status" label="Status">
+                <Select defaultValue="all">
+                  <Option value="all">All</Option>
+                  <Option value="draft">Draft</Option>
+                  <Option value="publish">Publish</Option>
+                  <Option value="ready">Ready to Publish</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={6} key={5}>
+              <Form.Item name="tags" label="Tags">
+                <Selector mode="multiple" action="Tags" placeholder="Filter Tags" />
+              </Form.Item>
+            </Col>
+            <Col span={6} key={6}>
+              <Form.Item name="categories" label="Categories">
+                <Selector mode="multiple" action="Categories" placeholder="Filter Categories" />
+              </Form.Item>
+            </Col>
+            <Col span={6} key={7}>
+              <Form.Item name="authors" label="Authors">
+                <Selector
+                  mode="multiple"
+                  action="Authors"
+                  placeholder="Filter Authors"
+                  display={'email'}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+        <PageList
+          actions={actions}
+          format={formats.article}
+          data={{
+            pages: pages,
+            total: total,
+            loading: loading,
+            tags: tags,
+            categories: categories,
+          }}
+          filters={filters}
+          setFilters={setFilters}
+          fetchPages={fetchPages}
+        />
       </Space>
     );
   return <FormatNotFound status="info" title="Article format not found" link="/formats/create" />;
