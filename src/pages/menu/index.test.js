5718cc6307f123416a49e246914ad070108150cc
Bhumi101097 <bhumi.patel@factly.in>
2021-06-30 18:06:25 +0530

    Fixed test cases for menu

--- src/pages/menu/index.test.js
+++ src/pages/menu/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { useDispatch, useSelector, Provider } from 'react-redux';
+import { useDispatch, Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { mount } from 'enzyme';
@@ -14,60 +14,90 @@ const mockStore = configureMockStore(middlewares);
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
   useDispatch: jest.fn(),
-  useSelector: jest.fn(),
 }));
 
 jest.mock('../../actions/menu', () => ({
   getMenus: jest.fn(),
 }));
-
+let state = {
+  menu: {
+    req: [
+      {
+        data: [1],
+        query: {
+          page: 1,
+          limit: 5,
+        },
+        total: 1,
+      },
+    ],
+    details: {
+      1: {
+        id: 1,
+        name: 'Menu 1',
+      },
+    },
+    loading: false,
+  },
+  spaces: {
+    orgs: [{ id: 1, organisation: 'Organisation 1', spaces: [11] }],
+    details: {
+      11: { id: 11, name: 'Space 11' },
+    },
+    loading: false,
+    selected: 11,
+  },
+};
 describe('Menu component', () => {
   let store;
   let mockedDispatch;
 
   beforeEach(() => {
     store = mockStore({});
-    store.dispatch = jest.fn(() => ({}));
+    store.dispatch = jest.fn();
     mockedDispatch = jest.fn();
     useDispatch.mockReturnValue(mockedDispatch);
   });
   it('should render the component', () => {
-    useSelector.mockImplementationOnce(() => ({}));
+    let state2 = {
+      menu: {
+        req: [],
+        details: {},
+        loading: false,
+      },
+      spaces: {
+        orgs: [{ id: 1, organisation: 'Organisation 1', spaces: [11] }],
+        details: {
+          11: { id: 11, name: 'Space 11' },
+        },
+        loading: false,
+        selected: 11,
+      },
+    };
+    store = mockStore(state2);
     const tree = mount(
       <Provider store={store}>
         <Router>
-          <Menu />
+          <Menu
+            permission={{
+              actions: ['admin'],
+            }}
+          />
         </Router>
       </Provider>,
     );
     expect(tree).toMatchSnapshot();
   });
   it('should render the component with data', () => {
-    useSelector.mockImplementationOnce(() => ({
-      menu: {
-        req: [
-          {
-            data: [1],
-            query: {
-              page: 1,
-              limit: 5,
-            },
-            total: 1,
-          },
-        ],
-        details: {
-          '1': {
-            id: 1,
-            name: 'Menu 1',
-          },
-        },
-        loading: false,
-      },
-    }));
+    store = mockStore(state);
     const tree = mount(
       <Provider store={store}>
         <Router>
-          <Menu />
+          <Menu
+            permission={{
+              actions: ['admin'],
+            }}
+          />
         </Router>
       </Provider>,
     );
