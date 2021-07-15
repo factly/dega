706965f69a513a525650870c5021eab0efefb9df
Bhumi101097 <bhumi.patel@factly.in>
2021-07-15 15:56:59 +0530

    Updated fact-checks testcases

--- studio/src/pages/fact-checks/components/FactCheckForm.test.js
+++ studio/src/pages/fact-checks/components/FactCheckForm.test.js
@@ -7,6 +7,7 @@ import { act } from '@testing-library/react';
 import { mount } from 'enzyme';
 import { useHistory } from 'react-router-dom';
 import moment from 'moment';
+import { Collapse, Dropdown } from 'antd';
 
 import '../../../matchMedia.mock';
 import FactCheckForm from './FactCheckForm';
@@ -16,6 +17,10 @@ import { addClaim } from '../../../actions/claims';
 
 Date.now = jest.fn(() => 1487076708000);
 jest.mock('@editorjs/editorjs');
+jest.mock('react-monaco-editor', () => {
+  const MonacoEditor = () => <div />;
+  return MonacoEditor;
+});
 
 jest.mock('./../../../actions/sidebar', () => ({
   setCollapse: jest.fn(),
@@ -59,6 +64,7 @@ const data = {
   tags: [1],
   authors: [1],
   claims: [1],
+  claim_order: [1],
   description: {
     time: 1595747741807,
     blocks: [
@@ -225,7 +231,7 @@ describe('Fact-check form component', () => {
         const settingButton = tree.find('Button').at(3);
         expect(settingButton.text()).toBe('');
         settingButton.simulate('click');
-        expect(tree.find('Drawer').length).toBe(1);
+        expect(tree.find('Drawer').length).not.toBe(0);
       });
       expect(tree).toMatchSnapshot();
     });
@@ -292,6 +298,21 @@ describe('Fact-check form component', () => {
     });
     it('should submit form with given data', (done) => {
       act(() => {
+        const settingButton = wrapper.find('Button').at(3);
+        expect(settingButton.text()).toBe('');
+        settingButton.simulate('click');
+      });
+      wrapper.update();
+      act(() => {
+        wrapper
+          .find('FormItem')
+          .at(8)
+          .find('Select')
+          .at(0)
+          .props()
+          .onChange({ target: { value: [3] } });
+      });
+      act(() => {
         const submitButtom = wrapper.find('Button').at(1);
         expect(submitButtom.text()).toBe('Save');
         submitButtom.simulate('click');
@@ -315,8 +336,9 @@ describe('Fact-check form component', () => {
           published_date: null,
           tag_ids: [1],
           tags: [1],
-          claims: [1],
-          claim_ids: [1],
+          claims: [3],
+          claim_ids: [],
+          claim_order: [],
           description: {
             time: 1595747741807,
             blocks: [
@@ -351,6 +373,24 @@ describe('Fact-check form component', () => {
           .simulate('change', { target: { value: 'New Title' } });
       });
       act(() => {
+        const settingButton = wrapper.find('Button').at(3);
+        expect(settingButton.text()).toBe('');
+        settingButton.simulate('click');
+        wrapper
+          .find('FormItem')
+          .at(8)
+          .find('Select')
+          .at(0)
+          .props()
+          .onChange({ target: { value: [1, 3] } });
+      });
+      act(() => {
+        const dropdown = wrapper.find(Dropdown);
+        dropdown.simulate('mouseover');
+        const overlay = dropdown.props().overlay;
+        overlay.props.children.props.children[1].props.onChange(true);
+      });
+      act(() => {
         const submitButtom = wrapper.find('Button').at(1);
         expect(submitButtom.text()).toBe('Save');
         submitButtom.simulate('click');
@@ -365,7 +405,7 @@ describe('Fact-check form component', () => {
           excerpt: 'excerpt of factcheck',
           slug: 'new-title',
           featured_medium_id: 1,
-          status: 'draft',
+          status: 'ready',
           format_id: 1,
           author_ids: [1],
           authors: [1],
@@ -374,8 +414,9 @@ describe('Fact-check form component', () => {
           published_date: null,
           tag_ids: [1],
           tags: [1],
-          claims: [1],
-          claim_ids: [1],
+          claims: [1, 3],
+          claim_order: [1, 3],
+          claim_ids: [1, 3],
           description: {
             time: 1595747741807,
             blocks: [
@@ -428,86 +469,94 @@ describe('Fact-check form component', () => {
         done();
       }, 0);
     });
-    // it('should add claim', (done) => {
-    //   useSelector.mockImplementationOnce(() => ({
-    //     claims: {
-    //       req: [
-    //         {
-    //           data: [1, 2],
-    //           total: 2,
-    //           query: {
-    //             page: 1,
-    //           },
-    //         },
-    //       ],
-    //       loading: false,
-    //       details: {
-    //         1: {
-    //           id: 1,
-    //           claim: 'Claim 1',
-    //         },
-    //         2: {
-    //           id: 2,
-    //           claim: 'Claim 2',
-    //         },
-    //       },
-    //     },
-    //     total: 2,
-    //     loading: false,
-    //   }));
-    //   act(() => {
-    //     const addClaimButton = wrapper.find('FormItem').at(8).find('Button');
-    //     expect(addClaimButton.text()).toBe('Add Claim');
-    //     addClaimButton.simulate('click');
-    //   });
-    //   wrapper.update();
+    it('should add claim', (done) => {
+      useSelector.mockImplementationOnce(() => ({
+        claims: {
+          req: [
+            {
+              data: [1, 2],
+              total: 2,
+              query: {
+                page: 1,
+              },
+            },
+          ],
+          loading: false,
+          details: {
+            1: {
+              id: 1,
+              claim: 'Claim 1',
+            },
+            2: {
+              id: 2,
+              claim: 'Claim 2',
+            },
+          },
+        },
+        total: 2,
+        loading: false,
+      }));
 
-    //   act(() => {
-    //     const claimForm = wrapper.find('Modal');
-    //     claimForm
-    //       .find('FormItem')
-    //       .at(0)
-    //       .find('TextArea')
-    //       .at(0)
-    //       .simulate('change', { target: { value: 'Claim title' } });
-    //     claimForm
-    //       .find('FormItem')
-    //       .at(3)
-    //       .find('Select')
-    //       .at(0)
-    //       .props()
-    //       .onChange({ target: { value: 1 } });
-    //     claimForm
-    //       .find('FormItem')
-    //       .at(4)
-    //       .find('Select')
-    //       .at(0)
-    //       .props()
-    //       .onChange({ target: { value: 1 } });
+      act(() => {
+        const addClaimButton = wrapper.find('FormItem').at(8).find('Button');
+        expect(addClaimButton.text()).toBe('Add Claim');
+        addClaimButton.simulate('click');
+      });
+      wrapper.update();
+      act(() => {
+        wrapper.find(Collapse).at(0).props().onChange();
+        wrapper.find(Collapse).at(1).props().onChange();
+      });
+      wrapper.update();
 
-    //     const nextButton = claimForm.find('Button').at(3);
-    //     nextButton.simulate('submit');
-    //   });
-    //   setTimeout(() => {
-    //     expect(addClaim).toHaveBeenCalled();
-    //     expect(addClaim).toHaveBeenCalledWith({
-    //       checked_date: null,
-    //       claim_date: null,
-    //       claim_sources: undefined,
-    //       claimant: 1,
-    //       claimant_id: 1,
-    //       description: undefined,
-    //       rating: 1,
-    //       rating_id: 1,
-    //       fact: undefined,
-    //       review_sources: undefined,
-    //       review_tag_line: undefined,
-    //       slug: 'claim-title',
-    //       claim: 'Claim title',
-    //     });
-    //     done();
-    //   }, 0);
-    // });
+      act(() => {
+        const claimForm = wrapper.find('Modal');
+        claimForm
+          .find('FormItem')
+          .at(0)
+          .find('TextArea')
+          .at(0)
+          .simulate('change', { target: { value: 'Claim title' } });
+        claimForm
+          .find('FormItem')
+          .at(3)
+          .find('Select')
+          .at(0)
+          .props()
+          .onChange({ target: { value: 1 } });
+        claimForm
+          .find('FormItem')
+          .at(4)
+          .find('Select')
+          .at(0)
+          .props()
+          .onChange({ target: { value: 1 } });
+
+        const nextButton = claimForm.find('Button').at(2);
+        expect(nextButton.text()).toBe('Submit');
+        nextButton.simulate('submit');
+      });
+      setTimeout(() => {
+        expect(addClaim).toHaveBeenCalled();
+        expect(addClaim).toHaveBeenCalledWith({
+          checked_date: null,
+          claim_date: null,
+          claim_sources: undefined,
+          claimant: 1,
+          claimant_id: 1,
+          description: undefined,
+          rating: 1,
+          rating_id: 1,
+          fact: undefined,
+          review_sources: undefined,
+          review_tag_line: undefined,
+          slug: 'claim-title',
+          claim: 'Claim title',
+          meta_fields: undefined,
+        });
+        done();
+      }, 0);
+    });
     it('should handle cancel click of Modal', () => {
       act(() => {
         const addClaimButton = wrapper.find('FormItem').at(8).find('Button');
@@ -515,7 +564,12 @@ describe('Fact-check form component', () => {
         addClaimButton.simulate('click');
       });
       wrapper.update();
-      const okButton = wrapper.find('Modal').find('Button').at(4);
+      act(() => {
+        wrapper.find(Collapse).at(0).props().onChange();
+        wrapper.find(Collapse).at(1).props().onChange();
+      });
+      wrapper.update();
+      const okButton = wrapper.find('Modal').find('Button').at(3);
       expect(okButton.text()).toBe('Cancel');
       okButton.simulate('click');
       expect(wrapper.find('Modal').at(0).props().visible).toBe(false);
@@ -527,7 +581,12 @@ describe('Fact-check form component', () => {
         addClaimButton.simulate('click');
       });
       wrapper.update();
-      const okButton = wrapper.find('Modal').find('Button').at(5);
+      act(() => {
+        wrapper.find(Collapse).at(0).props().onChange();
+        wrapper.find(Collapse).at(1).props().onChange();
+      });
+      wrapper.update();
+      const okButton = wrapper.find('Modal').find('Button').at(4);
       expect(okButton.text()).toBe('OK');
       okButton.simulate('click');
       expect(wrapper.find('Modal').at(0).props().visible).toBe(false);
@@ -537,7 +596,7 @@ describe('Fact-check form component', () => {
         const settingButton = wrapper.find('Button').at(3);
         expect(settingButton.text()).toBe('');
         settingButton.simulate('click');
-        expect(wrapper.find('Drawer').length).toBe(1);
+        expect(wrapper.find('Drawer').length).not.toBe(0);
         const closeButtonn = wrapper.find('DrawerChild').find('button').at(0);
         closeButtonn.simulate('click');
       });
@@ -556,6 +615,7 @@ describe('Fact-check form component', () => {
         tags: [1],
         authors: [1],
         claims: [1],
+        claim_order: [1],
         description: {
           time: 1595747741807,
           blocks: [
@@ -624,6 +684,7 @@ describe('Fact-check form component', () => {
           tag_ids: [1],
           tags: [1],
           claims: [1],
+          claim_order: [1],
           claim_ids: [1],
           description: {
             time: 1595747741807,
@@ -714,6 +775,7 @@ describe('Fact-check form component', () => {
           tag_ids: [],
           published_date: null,
           claim_ids: [],
+          claim_order: [],
           description: {
             time: 1595747741807,
             blocks: [
@@ -776,8 +838,9 @@ describe('Fact-check form component', () => {
           category_ids: [1],
           tag_ids: [1],
           tags: [1],
-          claims: [1],
+          claims: [1, undefined],
           claim_ids: [1],
+          claim_order: [1],
           description: {
             time: 1595747741807,
             blocks: [
@@ -797,6 +860,112 @@ describe('Fact-check form component', () => {
               },
             ],
             version: '2.18.0',
+          },
+        });
+        done();
+      }, 0);
+    });
+    it('should add meta data', (done) => {
+      const data2 = { ...data };
+      data2.meta_fields = {
+        sample: 'testing1',
+      };
+      data2.meta = {
+        meta: 'data1',
+      };
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <Router>
+              <FactCheckForm
+                actions={['publish']}
+                onCreate={props.onCreate}
+                data={data2}
+                format={{ id: 1, name: 'article', slug: 'article' }}
+              />
+            </Router>
+          </Provider>,
+        );
+      });
+      act(() => {
+        const settingButton = wrapper.find('Button').at(3);
+        expect(settingButton.text()).toBe('');
+        settingButton.simulate('click');
+        expect(wrapper.find('Drawer').length).not.toBe(0);
+      });
+      wrapper.update();
+      act(() => {
+        const addMetaDataBtn = wrapper.find('FormItem').at(16).find('Button').at(0);
+        expect(addMetaDataBtn.text()).toBe('Add Meta Data');
+        addMetaDataBtn.simulate('click');
+      });
+
+      act(() => {
+        const metaFieldData = wrapper.find('FormItem').at(18).find('MonacoEditor');
+        metaFieldData.props().onChange({
+          target: { value: '{"sample":"testing"}' },
+        });
+        const metaData = wrapper.find('FormItem').at(19).find('MonacoEditor');
+        metaData.props().onChange({
+          target: { value: '{"meta":"data"}' },
+        });
+      });
+
+      act(() => {
+        const backBtn = wrapper.find('FormItem').at(17).find('Button').at(0);
+        expect(backBtn.text()).toBe('');
+        backBtn.simulate('click');
+
+        const submitButtom = wrapper.find('Button').at(1);
+        expect(submitButtom.text()).toBe('Save');
+        submitButtom.simulate('click');
+        submitButtom.simulate('submit');
+        wrapper.update();
+      });
+      setTimeout(() => {
+        expect(props.onCreate).toHaveBeenCalledTimes(1);
+        expect(props.onCreate).toHaveBeenCalledWith({
+          title: 'FactCheck-1',
+          excerpt: 'excerpt of factcheck',
+          slug: 'factcheck-1',
+          featured_medium_id: 1,
+          status: 'draft',
+          format_id: 1,
+          author_ids: [1],
+          authors: [1],
+          categories: [1],
+          category_ids: [1],
+          published_date: null,
+          tag_ids: [1],
+          tags: [1],
+          claims: [1, undefined],
+          claim_ids: [1],
+          claim_order: [1],
+          description: {
+            time: 1595747741807,
+            blocks: [
+              {
+                type: 'header',
+                data: {
+                  text: 'Editor.js',
+                  level: 2,
+                },
+              },
+              {
+                type: 'paragraph',
+                data: {
+                  text:
+                    'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text.',
+                },
+              },
+            ],
+            version: '2.18.0',
+          },
+          meta: {
+            meta: 'data',
+          },
+          meta_fields: {
+            sample: 'testing',
           },
         });
         done();
