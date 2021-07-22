647341226176cf9bd7af755ac28d1198d32964ac
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-07-22 16:52:00 +0530

    add webpack config override

--- studio/config-overrides.js
+++ studio/config-overrides.js
@@ -1,10 +1,19 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-
+const AntDesignThemePlugin = require('antd-theme-webpack-plugin');
+const path = require('path');
+const options = {
+  antDir: path.join(__dirname, './node_modules/antd'),
+  stylesDir: path.join(__dirname, './src/styles'),
+  varFile: path.join(__dirname, './src/styles/variables.less'),
+  themeVariables: ['@primary-color'],
+  indexFileName: 'index.html',
+};
 module.exports = function override(config, env) {
   config.plugins.push(
     new MonacoWebpackPlugin({
       languages: ['json', 'html'],
     }),
+    new AntDesignThemePlugin(options),
   );
   return config;
 };
