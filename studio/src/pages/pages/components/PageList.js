1a7c96797fe823d962a1b053404b00b266861757
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-07-29 15:10:33 +0530

    update list ui for posts,factchecks and page list pages

--- studio/src/pages/pages/components/PageList.js
+++ studio/src/pages/pages/components/PageList.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
-import { Popconfirm, Button, List, Space, Tag } from 'antd';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { Popconfirm, Button, Space, Tag, Table } from 'antd';
+import {
+  EditOutlined,
+  DeleteOutlined,
+  CheckCircleOutlined,
+  ExceptionOutlined,
+  ClockCircleOutlined,
+  CloseOutlined,
+} from '@ant-design/icons';
 import { useDispatch } from 'react-redux';
 import { deletePage } from '../../../actions/pages';
 import { Link } from 'react-router-dom';
-import ImagePlaceholder from '../../../components/ErrorsAndImage/PlaceholderImage';
 import QuickEdit from '../../../components/List/QuickEdit';
 import moment from 'moment';
 
 function PageList({ actions, format, status, data, filters, setFilters, fetchPages }) {
   const dispatch = useDispatch();
   const [id, setID] = useState(0);
+  const [expandedRowKeys, setExpandedRowKeys] = useState([0]);
 
   const getTagList = (tagids) => {
     return tagids.map((id) => <Tag>{data.tags[id].name}</Tag>);
@@ -18,15 +25,136 @@ function PageList({ actions, format, status, data, filters, setFilters, fetchPag
   const getCategoryList = (catIds) => {
     return catIds.map((id) => <Tag>{data.categories[id].name}</Tag>);
   };
+  const getAuthorsList = (ids) => {
+    return ids?.map((id) => <span>{data.authors[id].display_name}</span>);
+  };
+  const columns = [
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+      width: 400,
+      render: (_, item) => (
+        <Link to={`/pages/${item.id}/edit`}>
+          <p style={{ fontSize: '1.125rem', fontWeight: 500 }}>{item.title}</p>
+          {/*
+          {item.published_date && (
+            <p style={{ color: 'CaptionText' }}>
+              Published on {moment(item.published_date).format('MMMM Do YYYY')}
+            </p>
+          )}
+          <p style={{ color: 'CaptionText' }}>by {getAuthorsList(item.authors)}</p> 
+          */}
+        </Link>
+      ),
+    },
+    {
+      title: 'Categories',
+      dataIndex: 'categories',
+      key: 'categories',
+      ellipsis: true,
+      render: (item) => {
+        return item.length > 0 ? getCategoryList(item) : null;
+      },
+    },
+    {
+      title: 'Tags',
+      dataIndex: 'tags',
+      key: 'tags',
+      ellipsis: true,
+      render: (item) => {
+        return getTagList(item);
+      },
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+      width: 200,
+      render: (status) => {
+        return status === 'publish' ? (
+          <Tag icon={<CheckCircleOutlined />} color="green">
+            Published
+          </Tag>
+        ) : status === 'draft' ? (
+          <Tag color="red" icon={<ExceptionOutlined />}>
+            Draft
+          </Tag>
+        ) : status === 'ready' ? (
+          <Tag color="gold" icon={<ClockCircleOutlined />}>
+            Ready to Publish
+          </Tag>
+        ) : null;
+      },
+    },
+    {
+      title: 'Quick Actions',
+      dataIndex: 'quick-actions',
+      fixed: 'right',
+      align: 'center',
+      width: 150,
+      render: (_, item, idx) => {
+        const isOpen = item.id === expandedRowKeys[0];
+        return (
+          <>
+            <div style={{ display: 'flex' }}>
+              <Button
+                disabled={!(actions.includes('admin') || actions.includes('update'))}
+                onClick={() => {
+                  isOpen ? setExpandedRowKeys([]) : setExpandedRowKeys([item.id]);
+                  return setID(item.id);
+                }}
+                style={{ margin: '0.5rem' }}
+              >
+                {isOpen ? <CloseOutlined /> : <EditOutlined />}
+              </Button>
+              <Popconfirm
+                title="Are you sure you want to delete this?"
+                onConfirm={() => dispatch(deletePage(item.id)).then(() => fetchPages())}
+                disabled={!(actions.includes('admin') || actions.includes('delete'))}
+              >
+                <Button
+                  disabled={!(actions.includes('admin') || actions.includes('delete'))}
+                  type="danger"
+                  style={{ margin: '0.5rem' }}
+                >
+                  <DeleteOutlined />
+                </Button>
+              </Popconfirm>
+            </div>
+          </>
+        );
+      },
+    },
+  ];
 
   return (
     <Space direction="vertical">
-      <List
-        bordered
-        className="post-list"
-        loading={data.loading}
-        itemLayout="vertical"
+      <Table
         dataSource={data.pages}
+        columns={columns}
+        rowKey={(record) => record.id}
+        expandable={{
+          expandIconColumnIndex: -1,
+          expandedRowKeys,
+          onExpand: (expanded, record) => {
+            let keys = [];
+            if (expanded) {
+              keys.push(record.id);
+            }
+
+            setExpandedRowKeys(keys);
+          },
+          expandedRowRender: (item) => (
+            <QuickEdit
+              data={item}
+              setID={setID}
+              slug={format.slug}
+              onQuickEditUpdate={() => setExpandedRowKeys([])}
+            />
+          ),
+          expandIcon: () => {},
+        }}
         pagination={{
           total: data.total,
           current: filters.page,
@@ -34,102 +162,6 @@ function PageList({ actions, format, status, data, filters, setFilters, fetchPag
           onChange: (pageNumber, pageSize) =>
             setFilters({ ...filters, page: pageNumber, limit: pageSize }),
         }}
-        renderItem={(item) => (
-          <List.Item
-            actions={
-              item.id !== id
-                ? [
-                    <Link
-                      style={{
-                        marginRight: 8,
-                      }}
-                      to={`/pages/${item.id}/edit`}
-                    >
-                      <Button
-                        icon={<EditOutlined />}
-                        disabled={!(actions.includes('admin') || actions.includes('update'))}
-                      >
-                        Edit
-                      </Button>
-                    </Link>,
-                    <Popconfirm
-                      title="Sure to Delete?"
-                      onConfirm={() => dispatch(deletePage(item.id)).then(() => fetchPages())}
-                      disabled={!(actions.includes('admin') || actions.includes('delete'))}
-                    >
-                      <Button
-                        icon={<DeleteOutlined />}
-                        disabled={!(actions.includes('admin') || actions.includes('delete'))}
-                      >
-                        Delete
-                      </Button>
-                    </Popconfirm>,
-                    <Button
-                      icon={<EditOutlined />}
-                      disabled={!(actions.includes('admin') || actions.includes('update'))}
-                      onClick={() => setID(item.id)}
-                    >
-                      Quick Edit
-                    </Button>,
-                    item.status === 'publish' ? (
-                      <Button style={{ border: 'solid 1px', color: 'green', width: '140px' }}>
-                        Published
-                      </Button>
-                    ) : item.status === 'draft' ? (
-                      <Button style={{ border: 'solid 1px', color: 'red', width: '140px' }}>
-                        Draft
-                      </Button>
-                    ) : item.status === 'ready' ? (
-                      <Button style={{ border: 'solid 1px', color: 'gold', width: '140px' }}>
-                        Ready to Publish
-                      </Button>
-                    ) : null,
-                  ]
-                : []
-            }
-            extra={
-              item.id !== id ? (
-                item.medium ? (
-                  <img
-                    style={{ width: '150', height: '150' }}
-                    alt={item.medium.alt_text}
-                    src={
-                      item.medium.url?.proxy
-                        ? `${item.medium.url.proxy}?resize:fill:150:150/gravity:sm`
-                        : ''
-                    }
-                  />
-                ) : (
-                  <ImagePlaceholder height={150} width={150} />
-                )
-              ) : null
-            }
-          >
-            {item.id !== id ? (
-              <List.Item.Meta
-                title={<Link to={`/pages/${item.id}/edit`}>{item.title}</Link>}
-                description={item.excerpt}
-              />
-            ) : null}
-            {item.id === id ? (
-              <QuickEdit data={item} setID={setID} slug={format.slug} page={true} />
-            ) : null}
-            {item.id !== id ? (
-              <Space direction="vertical">
-                {item.published_date ? (
-                  <div>Published Date: {moment(item.published_date).format('MMMM Do YYYY')}</div>
-                ) : null}
-                {item.tags && item.tags.length > 0 ? (
-                  <div>Tags: {getTagList(item.tags)}</div>
-                ) : null}
-
-                {item.categories && item.categories.length > 0 ? (
-                  <div>Categories: {getCategoryList(item.categories)}</div>
-                ) : null}
-              </Space>
-            ) : null}
-          </List.Item>
-        )}
       />
     </Space>
   );
