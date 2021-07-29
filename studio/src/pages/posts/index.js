bc2173f8ea9c3e3f079f5ac0979cca5268d68057
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-07-29 15:07:47 +0530

    update filters ui for posts,factchecks and page list pages

--- studio/src/pages/posts/index.js
+++ studio/src/pages/posts/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
 import { Space, Button, Form, Col, Row, Input, Select } from 'antd';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import PostList from '../../components/List';
 import getUserPermission from '../../utils/getUserPermission';
 import { useSelector, useDispatch } from 'react-redux';
@@ -10,8 +10,8 @@ import deepEqual from 'deep-equal';
 import { getPosts } from '../../actions/posts';
 import Selector from '../../components/Selector';
 import Template from '../../components/Template';
-import { useLocation } from 'react-router-dom';
 import getUrlParams from '../../utils/getUrlParams';
+import { UpOutlined, DownOutlined } from '@ant-design/icons';
 
 function Posts({ formats }) {
   const spaces = useSelector(({ spaces }) => spaces);
@@ -21,6 +21,34 @@ function Posts({ formats }) {
   const [form] = Form.useForm();
   const dispatch = useDispatch();
   const [formatFlag, setFormatFlag] = React.useState(false);
+  const [expand, setExpand] = React.useState(false);
+  const getFields = () => {
+    const children = [];
+    expand &&
+      children.push(
+        <Col span={8} key={5}>
+          <Form.Item name="tag" label="Tags">
+            <Selector mode="multiple" action="Tags" placeholder="Filter Tags" />
+          </Form.Item>
+        </Col>,
+        <Col span={8} key={6}>
+          <Form.Item name="category" label="Categories">
+            <Selector mode="multiple" action="Categories" placeholder="Filter Categories" />
+          </Form.Item>
+        </Col>,
+        <Col span={8} key={7}>
+          <Form.Item name="author" label="Authors">
+            <Selector
+              mode="multiple"
+              action="Authors"
+              placeholder="Filter Authors"
+              display={'email'}
+            />
+          </Form.Item>
+        </Col>,
+      );
+    return children;
+  };
   const history = useHistory();
 
   const keys = ['page', 'limit', 'q', 'sort', 'tag', 'category', 'author', 'format', 'status'];
@@ -57,7 +85,7 @@ function Posts({ formats }) {
     setFilters({ ...filters, format: [formats.article.id] });
     setFormatFlag(true);
   }
-  const { posts, total, loading, tags, categories } = useSelector((state) => {
+  const { posts, total, loading, tags, categories, authors } = useSelector((state) => {
     const node = state.posts.req.find((item) => {
       return deepEqual(item.query, params);
     });
@@ -74,8 +102,16 @@ function Posts({ formats }) {
         loading: state.posts.loading,
         tags: state.tags.details,
         categories: state.categories.details,
+        authors: state.authors.details,
       };
-    return { posts: [], total: 0, loading: state.posts.loading, tags: {}, categories: {} };
+    return {
+      posts: [],
+      total: 0,
+      loading: state.posts.loading,
+      tags: {},
+      categories: {},
+      authors: {},
+    };
   });
 
   React.useEffect(() => {
@@ -122,64 +158,61 @@ function Posts({ formats }) {
             }
           }}
         >
-          <Row gutter={24}>
-            <Col key={1}>
-              <Link to="/posts/create">
-                <Button disabled={!(actions.includes('admin') || actions.includes('create'))}>
-                  Create New
-                </Button>
-              </Link>
+          <Row justify="end" gutter={16} style={{ marginBottom: '1rem' }}>
+            <Col key={2} style={{ display: 'flex', justifyContent: 'end' }}>
+              <Form.Item name="q">
+                <Input placeholder="Search posts" />
+              </Form.Item>
+              <Form.Item>
+                <Button htmlType="submit">Search</Button>
+              </Form.Item>
             </Col>
-            <Col key={2} span={9} offset={12}>
-              <Space direction="horizontal">
-                <Form.Item name="q">
-                  <Input placeholder="Search posts" />
-                </Form.Item>
-
-                <Form.Item>
-                  <Button htmlType="submit">Search</Button>
-                </Form.Item>
-                <Form.Item name="sort" label="Sort">
-                  <Select defaultValue="desc" style={{ width: '100%' }}>
-                    <Option value="desc">Latest</Option>
-                    <Option value="asc">Old</Option>
-                  </Select>
-                </Form.Item>
-              </Space>
-            </Col>
-          </Row>
-          <Row gutter={10}>
-            <Col span={4} key={4}>
-              <Form.Item name="status" label="Status">
+            <Col key={4}>
+              <Form.Item name="status">
                 <Select defaultValue="all">
-                  <Option value="all">All</Option>
-                  <Option value="draft">Draft</Option>
-                  <Option value="publish">Publish</Option>
-                  <Option value="ready">Ready to Publish</Option>
+                  <Option value="all">Status: All</Option>
+                  <Option value="draft">Status: Draft</Option>
+                  <Option value="publish">Status: Publish</Option>
+                  <Option value="ready">Status: Ready to Publish</Option>
                 </Select>
               </Form.Item>
             </Col>
-            <Col span={6} key={5}>
-              <Form.Item name="tag" label="Tags">
-                <Selector mode="multiple" action="Tags" placeholder="Filter Tags" />
+            <Col>
+              <Form.Item name="sort">
+                <Select defaultValue="desc" style={{ width: '100%' }}>
+                  <Option value="desc">Sort By: Latest</Option>
+                  <Option value="asc">Sort By: Old</Option>
+                </Select>
               </Form.Item>
             </Col>
-            <Col span={6} key={6}>
-              <Form.Item name="category" label="Categories">
-                <Selector mode="multiple" action="Categories" placeholder="Filter Categories" />
-              </Form.Item>
-            </Col>
-            <Col span={6} key={7}>
-              <Form.Item name="author" label="Authors">
-                <Selector
-                  mode="multiple"
-                  action="Authors"
-                  placeholder="Filter Authors"
-                  display={'email'}
-                />
-              </Form.Item>
+            <Button
+              type="link"
+              onClick={() => {
+                setExpand(!expand);
+              }}
+            >
+              {expand ? (
+                <>
+                  Hide Filters <UpOutlined />
+                </>
+              ) : (
+                <>
+                  More Filters <DownOutlined />
+                </>
+              )}
+            </Button>
+            <Col key={1}>
+              <Link to="/posts/create">
+                <Button
+                  disabled={!(actions.includes('admin') || actions.includes('create'))}
+                  type="primary"
+                >
+                  New Post
+                </Button>
+              </Link>
             </Col>
           </Row>
+          <Row gutter={16}>{getFields()}</Row>
         </Form>
         <PostList
           actions={actions}
@@ -190,6 +223,7 @@ function Posts({ formats }) {
             loading: loading,
             tags: tags,
             categories: categories,
+            authors: authors,
           }}
           filters={filters}
           setFilters={setFilters}
