752c14ca601c86da09e5f86a3053677945578e82
shreeharsha-factly <harsha@factly.in>
2021-09-07 16:34:21 +0530

    feat: add GET_EVENT and UPDATE_EVENT and remove ADD_EVENT

--- studio/src/constants/events.js
+++ studio/src/constants/events.js
@@ -1,5 +1,6 @@
 //Actions
-export const ADD_EVENT = 'ADD_EVENT';
+export const GET_EVENT = 'GET_EVENT';
+export const UPDATE_EVENT = 'UPDATE_EVENT';
 export const ADD_EVENTS = 'ADD_EVENTS';
 export const ADD_EVENTS_REQUEST = 'ADD_EVENTS_REQUEST';
 export const RESET_EVENTS = 'RESET_EVENTS';
