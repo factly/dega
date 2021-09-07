43f1916e9f7e913ca4a27b1a1d7943289dade901
shreeharsha-factly <harsha@factly.in>
2021-09-07 16:21:36 +0530

    feat: add GET_EPSIODE and UPDATE_EPISODE and remove ADD_EPISODE

--- studio/src/constants/episodes.js
+++ studio/src/constants/episodes.js
@@ -1,5 +1,6 @@
 //Actions
-export const ADD_EPISODE = 'ADD_EPISODE';
+export const GET_EPISODE = 'GET_EPISODE';
+export const UPDATE_EPISODE = 'UPDATE_EPISODE';
 export const ADD_EPISODES = 'ADD_EPISODES';
 export const ADD_EPISODES_REQUEST = 'ADD_EPISODES_REQUEST';
 export const RESET_EPISODES = 'RESET_EPISODES';
