b85a5327c1e18b62f6fc536523c88c11b06e2205
tejveer777 <127482564+tejveer777@users.noreply.github.com>
2023-03-17 08:37:18 +0000

    fixed positionig of empty component

--- studio/src/components/GlobalNav/SpaceSelector.js
+++ studio/src/components/GlobalNav/SpaceSelector.js
@@ -121,14 +121,14 @@ function SpaceSelector({ onClose }) {
                   <Avatar
                     src={
                       details[item]?.logo?.url?.[
-                        window.REACT_APP_ENABLE_IMGPROXY ? 'proxy' : 'raw'
+                      window.REACT_APP_ENABLE_IMGPROXY ? 'proxy' : 'raw'
                       ] || degaImg
                     }
                   />
                 }
                 description={
                   searchquery &&
-                  details[item].name.toLowerCase().includes(searchquery.toLowerCase()) ? (
+                    details[item].name.toLowerCase().includes(searchquery.toLowerCase()) ? (
                     <Typography.Text strong>
                       {details[item].name
                         .split(new RegExp(`(${searchquery})`, 'gi'))
@@ -237,7 +237,9 @@ function SpaceSelector({ onClose }) {
                   );
                 })
               ) : (
-                <Empty />
+                <Col span={24}>
+                  <Empty />
+                </Col>
               )}
             </Row>
           </Col>
