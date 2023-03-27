75d8744f624ed3825ab4ec1926856c4285edd4c1
surajmn1 <suraj.natraj@factly.in>
2023-03-27 11:26:25 +0530

    update handlers to use medium plugin service

--- server/service/core/action/medium/list.go
+++ server/service/core/action/medium/list.go
@@ -1,15 +1,12 @@
 package medium
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 
-	"github.com/factly/dega-server/config"
 	"github.com/factly/dega-server/service/core/model"
+	"github.com/factly/dega-server/service/core/service"
 	"github.com/factly/x/errorx"
 	"github.com/factly/x/loggerx"
-	"github.com/factly/x/meilisearchx"
 	"github.com/factly/x/middlewarex"
 	"github.com/factly/x/paginationx"
 	"github.com/factly/x/renderx"
@@ -46,71 +43,13 @@ func list(w http.ResponseWriter, r *http.Request) {
 
 	searchQuery := r.URL.Query().Get("q")
 	sort := r.URL.Query().Get("sort")
-	var filteredMediumIDs []uint
-
-	result := paging{}
-	result.Nodes = make([]model.Medium, 0)
-
 	offset, limit := paginationx.Parse(r.URL.Query())
 
-	if sort != "asc" {
-		sort = "desc"
-	}
-
-	tx := config.DB.Model(&model.Medium{}).Where(&model.Medium{
-		SpaceID: uint(sID),
-	}).Order("created_at " + sort)
-
-	if searchQuery != "" {
-
-		if config.SearchEnabled() {
-			filters := fmt.Sprint("space_id=", sID)
-
-			var hits []interface{}
-
-			hits, err = meilisearchx.SearchWithQuery("dega", searchQuery, filters, "medium")
-			if err != nil {
-				loggerx.Error(err)
-				errorx.Render(w, errorx.Parser(errorx.NetworkError()))
-				return
-			}
-
-			filteredMediumIDs = meilisearchx.GetIDArray(hits)
-			if len(filteredMediumIDs) == 0 {
-				renderx.JSON(w, http.StatusOK, result)
-				return
-			} else {
-				err = tx.Where(filteredMediumIDs).Count(&result.Total).Offset(offset).Limit(limit).Find(&result.Nodes).Error
-				if err != nil {
-					loggerx.Error(err)
-					errorx.Render(w, errorx.Parser(errorx.DBError()))
-					return
-				}
-			}
-		} else {
-			if config.Sqlite() {
-				err = tx.Where("name LIKE ?", "%"+strings.ToLower(searchQuery)+"%").Or("description LIKE ?", "%"+strings.ToLower(searchQuery)+"%").Count(&result.Total).Offset(offset).Limit(limit).Find(&result.Nodes).Error
-				if err != nil {
-					loggerx.Error(err)
-					errorx.Render(w, errorx.Parser(errorx.DBError()))
-					return
-				}
-			} else {
-				err = tx.Where("name ILIKE ?", "%"+strings.ToLower(searchQuery)+"%").Or("description ILIKE ?", "%"+strings.ToLower(searchQuery)+"%").Count(&result.Total).Offset(offset).Limit(limit).Find(&result.Nodes).Error
-				if err != nil {
-					loggerx.Error(err)
-					errorx.Render(w, errorx.Parser(errorx.DBError()))
-					return
-				}
-			}
-		}
-	} else {
-		err = tx.Count(&result.Total).Offset(offset).Limit(limit).Find(&result.Nodes).Error
-		if err != nil {
-			loggerx.Error(err)
-			errorx.Render(w, errorx.Parser(errorx.DBError()))
-			return
-		}
+	mediumService := service.GetMediumService()
+	result, errMessages := mediumService.List(uint(sID), offset, limit, searchQuery, sort)
+	if errMessages != nil {
+		errorx.Render(w, errMessages)
+		return
 	}
 
 	renderx.JSON(w, http.StatusOK, result)
