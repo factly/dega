5ab356f72bcbe7e3fc9853a8f676d84fe5fdc3b9
elliot14A <akshithkatkuri14@gmail.com>
2023-03-28 23:20:47 +0530

    feat:claimant/create service complete

--- server/service/fact-check/service/claimant.go
+++ server/service/fact-check/service/claimant.go
@@ -2,11 +2,16 @@ package service
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/factly/dega-server/config"
 	"github.com/factly/dega-server/service/fact-check/model"
+	"github.com/factly/dega-server/util"
 	"github.com/factly/x/errorx"
+	"github.com/factly/x/loggerx"
+	"github.com/factly/x/slugx"
+	"github.com/factly/x/validationx"
 	"github.com/jinzhu/gorm/dialects/postgres"
 	"gorm.io/gorm"
 )
@@ -16,7 +21,7 @@ type paging struct {
 	Nodes []Claimant `json:"nodes"`
 }
 
-// Claimant model
+// claimant model
 type Claimant struct {
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
@@ -32,11 +37,13 @@ type Claimant struct {
 	FooterCode  string         `json:"footer_code"`
 }
 
+var userContext config.ContextKey = "claimant_user"
+
 type IClaimantService interface {
 	GetById(sID, id int) (model.Claimant, error)
 	List(sID uint, offset, limit int, searchQuery, sort string) (paging, []errorx.Message)
-	Create(ctx context.Context, sID, uID int, tag *Claimant) (model.Claimant, []errorx.Message)
-	Update(sID, uID, id int, tag *Claimant) (model.Claimant, []errorx.Message)
+	Create(ctx context.Context, sID, uID int, claimant *Claimant) (model.Claimant, []errorx.Message)
+	Update(sID, uID, id int, claimant *Claimant) (model.Claimant, []errorx.Message)
 	Delete(sID, id int) []errorx.Message
 }
 
@@ -45,8 +52,85 @@ type claimantService struct {
 }
 
 // Create implements IClaimantService
-func (claimantService) Create(ctx context.Context, sID int, uID int, tag *Claimant) (model.Claimant, []errorx.Message) {
-	panic("unimplemented")
+func (cs claimantService) Create(ctx context.Context, sID int, uID int, claimant *Claimant) (model.Claimant, []errorx.Message) {
+
+	validationErr := validationx.Check(claimant)
+	if validationErr != nil {
+		loggerx.Error(errors.New("validation error"))
+		return model.Claimant{}, validationErr
+	}
+
+	stmt := &gorm.Statement{DB: cs.model}
+	_ = stmt.Parse(&model.Claimant{})
+	tableName := stmt.Schema.Table
+
+	var claimantSlug string
+	if claimant.Slug != "" && slugx.Check(claimant.Slug) {
+		claimantSlug = claimant.Slug
+	} else {
+		claimantSlug = slugx.Make(claimant.Name)
+	}
+
+	mediumID := &claimant.MediumID
+	if claimant.MediumID == 0 {
+		mediumID = nil
+	}
+
+	// Check if claimant with same name exist
+	if util.CheckName(uint(sID), claimant.Name, tableName) {
+		loggerx.Error(errors.New(`claimant with same name exist`))
+		return model.Claimant{}, errorx.Parser(errorx.SameNameExist())
+	}
+
+	var descriptionHTML string
+	var err error
+	var jsonDescription postgres.Jsonb
+	if len(claimant.Description.RawMessage) > 0 {
+		descriptionHTML, err = util.GetDescriptionHTML(claimant.Description)
+		if err != nil {
+			loggerx.Error(err)
+			return model.Claimant{}, errorx.Parser(errorx.DecodeError())
+		}
+
+		jsonDescription, err = util.GetJSONDescription(claimant.Description)
+		if err != nil {
+			loggerx.Error(err)
+			return model.Claimant{}, errorx.Parser(errorx.DecodeError())
+		}
+	}
+
+	result := &model.Claimant{
+		Base: config.Base{
+			CreatedAt: claimant.CreatedAt,
+			UpdatedAt: claimant.UpdatedAt,
+		},
+		Name:            claimant.Name,
+		Slug:            slugx.Approve(&config.DB, claimantSlug, sID, tableName),
+		Description:     jsonDescription,
+		DescriptionHTML: descriptionHTML,
+		MediumID:        mediumID,
+		IsFeatured:      claimant.IsFeatured,
+		SpaceID:         uint(sID),
+		TagLine:         claimant.TagLine,
+		MetaFields:      claimant.MetaFields,
+		Meta:            claimant.Meta,
+		HeaderCode:      claimant.HeaderCode,
+		FooterCode:      claimant.FooterCode,
+	}
+
+	tx := config.DB.WithContext(context.WithValue(ctx, userContext, uID)).Begin()
+	err = tx.Model(&model.Claimant{}).Create(&result).Error
+
+	if err != nil {
+		tx.Rollback()
+		loggerx.Error(err)
+		return model.Claimant{}, errorx.Parser(errorx.DBError())
+	}
+
+	tx.Model(&model.Claimant{}).Preload("Medium").First(&result)
+	tx.Commit()
+
+	return *result, nil
 }
 
 // Delete implements IClaimantService
@@ -71,7 +155,7 @@ func (claimantService) List(sID uint, offset int, limit int, searchQuery string,
 }
 
 // Update implements IClaimantService
-func (claimantService) Update(sID int, uID int, id int, tag *Claimant) (model.Claimant, []errorx.Message) {
+func (claimantService) Update(sID int, uID int, id int, claimant *Claimant) (model.Claimant, []errorx.Message) {
 	panic("unimplemented")
 }
 
