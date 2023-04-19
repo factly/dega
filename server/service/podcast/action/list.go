014e14b585cf79672e970e2ab71144cf81f90351
shreeharsha-factly <harsha@factly.in>
2023-04-19 12:15:21 +0530

    remove unused types

--- server/service/podcast/action/list.go
+++ server/service/podcast/action/list.go
@@ -1,28 +1,16 @@
 package podcast
 
 import (
-	"fmt"
 	"net/http"
 	"net/url"
-	"strings"
 
-	"github.com/factly/dega-server/config"
-	"github.com/factly/dega-server/service/podcast/model"
 	"github.com/factly/dega-server/service/podcast/service"
 	"github.com/factly/x/errorx"
 	"github.com/factly/x/loggerx"
-	"github.com/factly/x/meilisearchx"
 	"github.com/factly/x/middlewarex"
 	"github.com/factly/x/paginationx"
 	"github.com/factly/x/renderx"
-	"gorm.io/gorm"
 )
-
-// list response
-type paging struct {
-	Total int64           `json:"total"`
-	Nodes []model.Podcast `json:"nodes"`
-}
 
 // list - Get all podcasts
 // @Summary Show all podcasts
@@ -71,68 +59,4 @@ func list(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	renderx.JSON(w, http.StatusOK, result)
-}
-
-func generateFilters(categoryIDs, primaryCatID, language []string) string {
-	filters := ""
-	if len(categoryIDs) > 0 {
-		filters = fmt.Sprint(filters, meilisearchx.GenerateFieldFilter(categoryIDs, "category_ids"), " AND ")
-	}
-
-	if len(primaryCatID) > 0 {
-		filters = fmt.Sprint(filters, meilisearchx.GenerateFieldFilter(primaryCatID, "primary_category_id"), " AND ")
-	}
-
-	if len(language) > 0 {
-		filters = fmt.Sprint(filters, meilisearchx.GenerateFieldFilter(language, "language"), " AND ")
-	}
-
-	if filters != "" && filters[len(filters)-5:] == " AND " {
-		filters = filters[:len(filters)-5]
-	}
-
-	return filters
-}
-
-func generateSQLFilters(tx *gorm.DB, searchQuery string, categoryIDs, primaryCatID, language []string) string {
-	filters := ""
-	if config.Sqlite() {
-		if searchQuery != "" {
-			filters = fmt.Sprint(filters, "title LIKE '%", strings.ToLower(searchQuery), "%' AND ")
-		}
-	} else {
-		if searchQuery != "" {
-			filters = fmt.Sprint(filters, "title ILIKE '%", strings.ToLower(searchQuery), "%' AND ")
-		}
-	}
-	if len(primaryCatID) > 0 {
-		filters = filters + " primary_category_id IN ("
-		for _, id := range primaryCatID {
-			filters = fmt.Sprint(filters, id, ", ")
-		}
-		filters = fmt.Sprint("(", strings.Trim(filters, ", "), ")) AND ")
-	}
-
-	if len(language) > 0 {
-		filters = filters + " language IN ("
-		for _, lan := range language {
-			filters = fmt.Sprint(filters, "'", lan, "'", ", ")
-		}
-		filters = fmt.Sprint("(", strings.Trim(filters, ", "), ")) AND ")
-	}
-
-	if len(categoryIDs) > 0 {
-		tx.Joins("INNER JOIN podcast_categories ON podcasts.id = podcast_categories.podcast_id")
-		filters = filters + " podcast_categories.category_id IN ("
-		for _, id := range categoryIDs {
-			filters = fmt.Sprint(filters, id, ", ")
-		}
-		filters = fmt.Sprint("(", strings.Trim(filters, ", "), ")) AND ")
-	}
-
-	if filters != "" && filters[len(filters)-5:] == " AND " {
-		filters = filters[:len(filters)-5]
-	}
-
-	return filters
 }
--- server/service/podcast/action/route.go
+++ server/service/podcast/action/route.go
@@ -1,32 +1,9 @@
 package podcast
 
 import (
-	"time"
-
-	"github.com/factly/dega-server/config"
 	"github.com/factly/dega-server/util"
 	"github.com/go-chi/chi"
-	"github.com/jinzhu/gorm/dialects/postgres"
 )
-
-// podcast model
-type podcast struct {
-	CreatedAt         time.Time      `json:"created_at"`
-	UpdatedAt         time.Time      `json:"updated_at"`
-	Title             string         `json:"title"  validate:"required,max=500"`
-	Slug              string         `json:"slug"`
-	Language          string         `json:"language"  validate:"required"`
-	Description       postgres.Jsonb `json:"description" swaggertype:"primitive,string"`
-	MediumID          uint           `json:"medium_id"`
-	SpaceID           uint           `json:"space_id"`
-	PrimaryCategoryID uint           `json:"primary_category_id"`
-	CategoryIDs       []uint         `json:"category_ids"`
-	HeaderCode        string         `json:"header_code"`
-	FooterCode        string         `json:"footer_code"`
-	MetaFields        postgres.Jsonb `json:"meta_fields" swaggertype:"primitive,string"`
-}
-
-var podcastUser config.ContextKey = "podcast_user"
 
 // Router - Group of podcast router
 func Router() chi.Router {
