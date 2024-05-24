9a2cdcb64c3a590de764ef4b09183d5c67c150d0
ganga2166 <mukkagangaprasad@gmail.com>
2024-05-24 13:32:35 +0530

    modified buid-nocache.sh file

--- build-nocache.sh
+++ build-nocache.sh
@@ -5,4 +5,4 @@ CACHEBUST=$(date +%s)
 
 # Pass the cache bust value as a build argument
 docker-compose build --no-cache --build-arg CACHEBUST=$CACHEBUST dega-studio
-# docker-compose up
+docker-compose up
