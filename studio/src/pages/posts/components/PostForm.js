f3e8f2148bf0bcc3a3aac2a29c57101be154ebec
Pallavi Mishra <pallavi.mishra@factly.in>
2024-07-29 14:08:22 +0530

    Checking if author is selected when future publish is selected

--- studio/src/pages/posts/components/PostForm.js
+++ studio/src/pages/posts/components/PostForm.js
@@ -151,14 +151,15 @@ function PostForm({ onCreate, data = {}, actions = {}, format, page = false }) {
     finalData.format_id = format.id;
     finalData.author_ids = finalData.authors || [];
     finalData.status = status;
-    if (status === 'publish' && finalData.author_ids.length === 0) {
-      dispatch(addErrorNotification('At least one author must be assigned before publishing.'));
+    if ((status ==='publish' || status === 'future') && finalData.author_ids.length === 0) {
+      dispatch(addErrorNotification('At least one author must be assigned.'));
       return;
     }
-    if (status === 'future' && !finalData.published_date) {
+    if (status === 'future' ) {
+      if (!finalData.published_date) {
       dispatch(addErrorNotification('Published date is required for future publishing.'));
       return;
-    }
+    } }
     finalData.status === 'publish'
       ? (finalData.published_date = finalData.published_date
           ? dayjs(finalData.published_date).format('YYYY-MM-DDTHH:mm:ssZ')
