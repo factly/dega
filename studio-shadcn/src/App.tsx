9e0d1bf9609763561fa3dc6a1671039de251f4ee
surajmn1 <suraj.natraj@factly.in>
2025-02-18 13:59:04 +0530

    studio: init shadcn ts vite

---
+++ studio-shadcn/src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button variant="outline">Button</Button>
+    </>
+  )
+}
+
+export default App
