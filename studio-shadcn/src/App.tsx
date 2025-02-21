c137ea16210f4c2a767f6153c1555b0e25d8f124
rapoler <ramsai.rapole@factly.in>
2025-02-21 11:47:38 +0530

    resolve merge conflicts

--- studio-shadcn/src/App.tsx
+++ studio-shadcn/src/App.tsx
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-
-import { Button } from "@/components/ui/button"
-import './App.css'
-
-function App() {
-  return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button variant="outline">Button</Button>
-    </>
-  )
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { BasicLayout } from "./layouts/basic";
 import { routes } from "./config/routesConfig";
@@ -38,7 +23,6 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
->>>>>>> Stashed changes
 }
 
 export default App
